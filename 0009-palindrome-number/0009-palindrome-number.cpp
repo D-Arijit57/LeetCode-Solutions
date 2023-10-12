eec0e2407f6a55fd03f91ed7ec46075890ea5875
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2023-10-12 21:21:33 +0530

    Time: 12 ms (31.05%), Space: 6.2 MB (36.08%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int duplicate = x;
+        long long rev = 0 ;
+        while(x>0){
+            int r = x%10;
+            rev = rev * 10 + r;
+            x = x/10;
+        }
+        if(rev == duplicate )return true ;
+        return false;
+
+
+    }
+};
