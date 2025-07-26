8f951fe8baa5751651cc13dd794740c6b4bada5c
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2025-07-26 17:31:27 +0530

    Time: 507 ms (22.03%) | Memory: 401.5 MB (5.09%) - LeetSync

--- 3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.cpp
+++ 3789-maximize-subarrays-after-removing-one-conflicting-pair/maximize-subarrays-after-removing-one-conflicting-pair.cpp
@@ -1,30 +1,52 @@
 class Solution {
 public:
     long long maxSubarrays(int n, vector<vector<int>>& conflictingPairs) {
-        vector<int> bMin1(n + 1, INT_MAX), bMin2(n + 1, INT_MAX);
-        for (const auto& pair : conflictingPairs) {
-            int a = min(pair[0], pair[1]), b = max(pair[0], pair[1]);
-            if (bMin1[a] > b) {
-                bMin2[a] = bMin1[a];
-                bMin1[a] = b;
-            } else if (bMin2[a] > b) {
-                bMin2[a] = b;
-            }
+int m = conflictingPairs.size();
+        vector<vector<pair<int,int>>> edges(n+2);
+        for (int i = 0; i < m; i++) {
+            int a = conflictingPairs[i][0];
+            int b = conflictingPairs[i][1];
+            if (a > b) swap(a, b);
+            edges[a].push_back({b, i});
         }
-        long long res = 0;
-        int ib1 = n, b2 = INT_MAX;
-        vector<long long> delCount(n + 1, 0);
-        for (int i = n; i >= 1; i--) {
-            if (bMin1[ib1] > bMin1[i]) {
-                b2 = min(b2, bMin1[ib1]);
-                ib1 = i;
+        
+        multiset<pair<int,int>> ms;
+        vector<int> first_b(n+2), first_id(n+2), second_b(n+2);
+        for (int l = n; l >= 1; l--) {
+            for (auto &p : edges[l]) {
+                ms.insert(p);
+            }
+            if (!ms.empty()) {
+                auto it = ms.begin();
+                first_b[l] = it->first;
+                first_id[l] = it->second;
+                auto it2 = next(it);
+                second_b[l] = (it2 != ms.end() ? it2->first : n+1);
             } else {
-                b2 = min(b2, bMin1[i]);
+                first_b[l] = n+1;
+                first_id[l] = -1;
+                second_b[l] = n+1;
             }
-            res += min(bMin1[ib1], n + 1) - i;
-            delCount[ib1] +=
-                min(min(b2, bMin2[ib1]), n + 1) - min(bMin1[ib1], n + 1);
         }
-        return res + *max_element(delCount.begin(), delCount.end());
+        
+        long long sum1 = 0;
+        vector<long long> sum1k(m), sum2k(m);
+        for (int l = 1; l <= n; l++) {
+            long long d1 = (long long)first_b[l] - l;
+            long long d2 = (long long)second_b[l] - l;
+            sum1 += d1;
+            int id = first_id[l];
+            if (id >= 0) {
+                sum1k[id] += d1;
+                sum2k[id] += d2;
+            }
+        }
+        
+        long long ans = 0;
+        for (int i = 0; i < m; i++) {
+            long long now = sum1 - sum1k[i] + sum2k[i];
+            if (now > ans) ans = now;
+        }
+        return ans;
     }
 };
