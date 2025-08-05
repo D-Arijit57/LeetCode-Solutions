8ba0be20cbe1d7b90b9275487eb76b923bb77631
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2025-08-05 10:54:34 +0530

    Time: 4 ms (34.91%) | Memory: 19.8 MB (13.00%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        // bucket sort approach 
         int n = nums.size();
         unordered_map<int, int> freq;
-        for(int i = 0 ; i < n ; i++) {
+        for (int i = 0; i < n; i++) {
             freq[nums[i]]++;
         }
+        // using bucket sort
+        // creating a bucket
+        // indices -> frequency of elements (at max the size can be n)
         vector<vector<int>> bucket(n + 1);
-        
-        for(auto& pair : freq) {
+        // taking n+1 since the index starts from zero and we wanna take the
+        // same size as the array
+        for (auto const& pair : freq) {
             int elem = pair.first;
-            int freq = pair.second;
-            bucket[freq].push_back(elem);
+            int cnt = pair.second;
+            bucket[cnt].push_back(
+                elem); // storing the element corresponding to the count
         }
+
+        // creating the answer array
         vector<int> ans;
-        for(int i = n; i >= 0 && ans.size() < k; i--) {
-            for(int elem : bucket[i]) {
-                ans.push_back(elem);
-                if(ans.size() == k) {
+
+        // reiterate the bucket from the back
+        for (int i = n; i >= 0 && ans.size() < k; i--) {
+            for (int it : bucket[i]) {
+                ans.push_back(it);
+                if (ans.size() == k)
                     break;
-                }
             }
         }
-        
         return ans;
     }
-};
+    };
