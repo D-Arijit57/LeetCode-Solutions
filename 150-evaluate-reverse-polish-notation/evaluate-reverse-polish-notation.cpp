ddc5b876a80dc472286aa0f7c30c90f63fe79079
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2025-08-25 12:23:23 +0530

    Time: 3 ms (38.57%) | Memory: 16.9 MB (91.95%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         int n = tokens.size();
+        // using a stack 
         stack<int>st;
         for(int i = 0 ; i < n ; i++){
+            // if the string is an operator 
             if(tokens[i] == "*" || tokens[i] == "+" || tokens[i] == "/" || tokens[i] == "-"){
+                // takeout the two elements 
                 int elem1 = st.top();st.pop();
                 int elem2 = st.top();st.pop();
                 int ans = 0;
+                // perform the operation according to the string 
                 if(tokens[i] == "+"){
                     ans += elem2 + elem1;
                     st.push(ans);
@@ -25,10 +29,12 @@ public:
                     st.push(ans);
                 }
             }
+            // if its a number , then push it into the stack 
             else {
                 st.push(stoi(tokens[i]));
             }
         }
+        // return the top of the stack since only one element is left
         return st.top();
     }
 };
