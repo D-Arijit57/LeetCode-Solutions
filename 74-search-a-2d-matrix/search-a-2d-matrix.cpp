4ae4bfba1ccec032e9a1635e6d1af0f980d0bf3b
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2025-09-09 10:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.46%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size() , m = matrix[0].size();
-        int low = 0 , high = (n*m-1);
-        while(low <= high){
-            int mid =low + (high - low) / 2;
-            int row = mid/m , col = mid % m;
-            if(matrix[row][col] == target) return true;
-            else if (matrix[row][col]<target) low = mid + 1;
-            else high = mid - 1;
-        }
-        return false;
+      // get the size of the row and col
+      int n = matrix.size() , m = matrix[0].size();
+      // assign the two pointers in the start and in the end
+      int low = 0 , high = (n*m - 1);
+      while(low <= high){
+        int mid = low + (high - low) / 2;
+        // calculate the row and col 
+        int row = mid / m , col = mid % m;
+        if(matrix[row][col] == target) return true; // if the element is found return true
+        else if(matrix[row][col] > target) high = mid - 1; // if the element is greater than the target decrease right pointer
+        else low = mid + 1;// if the element is smaller then increase the left pointer
+      }
+      return false ; // since the element is not found return false
     }
 };
