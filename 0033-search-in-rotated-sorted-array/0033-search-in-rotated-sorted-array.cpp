33966b6d0edcc280bebc25a44e7e8085f96fc48d
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2025-10-07 10:14:31 +0530

    Time: 0 ms (100%), Space: 15.2 MB (65.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int low = 0 , high = n-1;
-        while(low<=high){
-            int mid = (low+high) / 2;
-            if(nums[mid] == target) return mid;
-            if(nums[low] <= nums[mid]){
-                if(nums[low] <= target && target <= nums[mid]){
-                    high = mid - 1;
-                }
-                else low = mid + 1;
-            }
-            else {
-                if(nums[mid] <= target && target <= nums[high]){
-                    low = mid + 1;
-                }
-                else high = mid - 1;
-            }
+       int n = nums.size();
+       int low = 0 , high = n - 1;
+       while(low <= high){
+        int mid = low + (high - low) / 2;
+        if(target == nums[mid]) return mid;
+        // if the element doesn't exist in the left half eliminate the left half
+        if(nums[low] <= nums[mid]){
+        if(target > nums[mid] || target < nums[low]){
+            low = mid + 1;
         }
-        return -1;
+        else high = mid - 1;
+        }
+        // if the element doesn't exist in the right half eliminate the right half
+        else {
+            if(target < nums[mid] || target > nums[high]){
+             high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        
+       }
+       return -1;
     }
 };
