43daa8cf4615ce609314cfb4fb67633840945cf8
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-01-09 10:05:31 +0530

    Time: 55 ms (32.91%) | Memory: 31.1 MB (24.68%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,33 +1,28 @@
-#include <vector>
-#include <algorithm>
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> res;
-
-        for (int i = 0; i < nums.size() - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1, right = nums.size() - 1;
-            while (left < right) {
-                int total = nums[i] + nums[left] + nums[right];
-                if (total == 0) {
-                    res.push_back({nums[i], nums[left], nums[right]});
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+        sort(nums.begin() , nums.end());
+        vector<vector<int>>res;
+        int n = nums.size();
+        for(int i = 0 ; i < n ; i++){
+            // check for same element beforehand to avoid duplicates
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i + 1 , right = n - 1;
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum > 0) right--;
+                else if(sum < 0) left++;
+                if(sum == 0){
+                    vector<int>triplets = {nums[i],nums[left],nums[right]};
+                    res.push_back(triplets);
+                    // to avoid the repeatation of same element by left and right
+                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    while(left < right && nums[left] == nums[left + 1]) left++;
                     left++;
-                    right--;
-                } else if (total < 0) {
-                    left++;
-                } else {
                     right--;
                 }
             }
         }
-
         return res;
     }
 };
