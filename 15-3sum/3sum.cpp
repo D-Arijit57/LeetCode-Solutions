8dae4b8b06a0008a54ba0bbe21110a558099ef07
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-01-09 10:08:17 +0530

    Time: 63 ms (22.02%) | Memory: 30.9 MB (27.82%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,6 +6,7 @@ public:
         int n = nums.size();
         for(int i = 0 ; i < n ; i++){
             // check for same element beforehand to avoid duplicates
+            // [2,2,.....] i =0, i=1 would create the same triplets 
             if(i > 0 && nums[i] == nums[i-1]) continue;
             int left = i + 1 , right = n - 1;
             while(left < right){
@@ -16,6 +17,8 @@ public:
                     vector<int>triplets = {nums[i],nums[left],nums[right]};
                     res.push_back(triplets);
                     // to avoid the repeatation of same element by left and right
+                    // [2,0,-1,-1,5,6,7,-4,-4] left = 2, right = 7 would create the same triplets
+                    // we need to skip the same element pointed by right and left
                     while(left < right && nums[right] == nums[right - 1]) right--;
                     while(left < right && nums[left] == nums[left + 1]) left++;
                     left++;
@@ -24,7 +27,5 @@ public:
             }
         }
         return res;
-        auto init = atexit([]()
-{ofstream ("display_runtime.txt")<<"0";});
     }
 };
