4a3970e67df3eac764b1af05a562d1d304361302
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-01-10 10:55:22 +0530

    Time: 8 ms (77.9%), Space: 13.9 MB (92.28%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -5,13 +5,15 @@ public:
         int n = nums.size();
         int minDiff = INT_MAX, ans = 0;
         for(int i = 0 ; i < n ; i++){
-            if( i > 0 && nums[i] == nums[i-1]) continue;
+            if( i > 0 && nums[i] == nums[i-1]) continue; // skipping the duplicates
             int left = i + 1, right = n - 1;
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum == target){
                     return sum;
                 }
+                // since there can be negatives we will use abs function 
+                // as we know only the distance matters irrespective of the sign convention
                 int currDiff = abs(sum - target);
                 if(currDiff < minDiff){
                     ans = sum;
