71ac2ff33d2f90c1d3436513da19cf27797cc15d
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-01-10 11:01:27 +0530

    Time: 8 ms (77.9%), Space: 14.1 MB (27.57%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,32 +1,59 @@
 class Solution {
+
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
+
+        sort(nums.begin(), nums.end());
+
         int n = nums.size();
-        int minDiff = INT_MAX, ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            if( i > 0 && nums[i] == nums[i-1]) continue; // skipping the duplicates
+
+        long long minDiff = INT_MAX, ans = 0;
+
+        for (int i = 0; i < n; i++) {
+
             int left = i + 1, right = n - 1;
-            while(left < right){
+
+            while (left < right) {
+
                 int sum = nums[i] + nums[left] + nums[right];
-                if(sum == target){
+
+                if (sum == target) {
+
                     return sum;
+
                 }
-                // since there can be negatives we will use abs function 
-                // as we know only the distance matters irrespective of the sign convention
-                int currDiff = abs(sum - target);
-                if(currDiff < minDiff){
-                    ans = sum;
-                    minDiff = currDiff;
+
+                else if (sum > target) {
+
+                    long long diff =(long long)(abs(sum - target)) ;
+
+                    if (diff < minDiff) {
+
+                        ans = sum;
+
+                        minDiff = diff;
+                    }
+
+                    right--;
+
                 }
-                else if(sum > target){
-                     right--;
-                }
-                else{
+
+                else {
+
+                     long long diff = (long long)(abs(sum - target)) ;
+
+                    if (diff < minDiff) {
+
+                        ans = sum;
+
+                        minDiff = diff;
+                    }
+
                     left++;
                 }
             }
         }
+
         return ans;
     }
 };
