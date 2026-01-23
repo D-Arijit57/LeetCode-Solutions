e4a174acf34401083d12bcb57b8546e306da4ad6
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-01-23 10:52:47 +0530

    Time: 0 ms (100%), Space: 7.6 MB (96.11%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -10,6 +10,11 @@ public:
         return sum;
     }
     bool isHappy(int n) {
+        // fast moves +2 ahead and checks if its 1 or same as fast 
+        // if its not a happy number it's bound to repeat 
+        // it creates a cyclic structure 
+        // so we visualize it as a linkedlist to find out using slow and fast pointers
+        // if there's a cycle (not a happy number) slow and fast are bound to meet each other
         int slow = n,fast = n;
         while(true){
          slow = calculate_sumOfdigits(slow);
