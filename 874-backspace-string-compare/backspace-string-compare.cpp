3fdc41a3dc17d6a8556f10b32fbe5d50d387c6ef
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-01-24 11:58:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (95.47%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -1,46 +1,41 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int n1 = s.size();
-        int n2 = t.size();
+        int n1 = s.size() , n2 = t.size();
         int i = n1 - 1 , j = n2 - 1;
-        int skipS = 0 , skipT = 0;
-        while( i >= 0 || j >= 0){
-            // for each character in string 's'
+        // cnt's for the #'s
+        int skipS = 0,skipT = 0;
+        while(i >= 0 || j >= 0){
             while(i >= 0){
-                // count the number of '#'s if there's any
                 if(s[i] == '#'){
                     skipS++;
                     i--;
                 }
-                // skip the #'s 
                 else if(skipS > 0){
-                    i--;
                     skipS--;
+                    i--;
                 }
                 else break;
             }
-            // do the same for string 't'
             while(j >= 0){
-                if(t[j] == '#'){
-                    skipT++;
-                    j--;
-                }
-                else if(skipT > 0){
-                    skipT--;
-                    j--;
-                }
-                else break;
+                    if(t[j] == '#'){
+                        skipT++;
+                        j--;
+                    }
+                    else if(skipT > 0){
+                        skipT--;
+                        j--;
+                    }
+                    else break;
             }
-            // after every skip , check if the two pointers are pointing the same character 
             if(i >= 0 && j >= 0){
                 if(s[i] != t[j]) return false;
             }
-            // in case one pointer finishes iteration before the other one 
-            else if(i >= 0 || j >= 0) return false;
+            // incase one pointer finishes first means both string are not of same length 
+            if((i >= 0)  !=  (j >= 0)) return false;
             i--;
             j--;
-        }
+        } 
         return true;
     }
 };
