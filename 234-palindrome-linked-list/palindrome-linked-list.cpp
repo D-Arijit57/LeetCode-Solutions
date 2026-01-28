52328ca550714aaf53a9c6ef7683977d1b19bf15
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-01-28 11:44:44 +0530

    Time: 1 ms (68.84%) | Memory: 117.9 MB (93.08%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,6 +11,7 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
+     if(!head || !head->next) return true;
      ListNode* slow = head;
      ListNode* fast = head;
      // first phase finding the middle of the linked list
@@ -23,10 +24,14 @@ public:
         // since we need to reverse the ll in place we are going to use curr, prev and next
         // reversing before the middle part
         ListNode* curr = slow;
-        ListNode* prev = nullptr; 
-        ListNode* next;
+        ListNode* prev = nullptr;
+        // Reversing the second half 
         while(curr != nullptr){
-            next = curr->next;
+            // temporarily storing the next node to be reversed
+            // to access it after reversing the current node 
+            ListNode* next = curr->next;
+            // connect the next pointer to the previous node
+            // prev becomes curr since its has already been reversed 
             curr->next = prev;
             prev = curr;
             curr = next;
@@ -34,6 +39,8 @@ public:
         // comparing the first half with the second half 
         ListNode* first = head;
         ListNode* second = prev;
+        // since the second part actually defines if its a palindrome or not
+        // meaning it would define how many comparisons are supposed to be done 
         while(second != nullptr){
             if(first->val != second->val) return false;
             first = first->next;
