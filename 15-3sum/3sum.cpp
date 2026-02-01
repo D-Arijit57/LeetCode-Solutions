a28805a6d3ccb9fe9b5781ddc6d08ec889ae8600
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-01 16:43:03 +0530

    Time: 52 ms (38.98%) | Memory: 29 MB (70.85%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
-        vector<vector<int>>res;
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        for(int i = 0 ; i < n ; i++){
-            // check for same element beforehand to avoid duplicates
-            // [2,2,.....] i =0, i=1 would create the same triplets 
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+        vector<vector<int>>ans;
+        // since we are checking triplets it makes no sense to iterate till n
+        // we can just iterate till n - 2 positions    
+        for(int i = 0 ; i < n-2 ; i++){
+            // skipping duplicates since it would lead to duplicate triplets
+            if(i > 0 && nums[i - 1] == nums[i]) continue;
             int left = i + 1 , right = n - 1;
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum > 0) right--;
-                else if(sum < 0) left++;
-                if(sum == 0){
-                    vector<int>triplets = {nums[i],nums[left],nums[right]};
-                    res.push_back(triplets);
-                    // to avoid the repeatation of same element by left and right
-                    // [2,0,-1,-1,5,6,7,-4,-4] left = 2, right = 7 would create the same triplets
-                    // we need to skip the same element pointed by right and left
-                    while(left < right && nums[right] == nums[right - 1]) right--;
+                else if(sum < 0) left++; 
+                else { // when sum is equal to the target
+                    ans.push_back({nums[i],nums[left],nums[right]});
+                    // skipping the duplicates
                     while(left < right && nums[left] == nums[left + 1]) left++;
+                    while(left < right && nums[right] == nums[right - 1]) right--;
                     left++;
                     right--;
                 }
             }
         }
-        return res;
+        return ans;
     }
 };
