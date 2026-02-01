a7e6165206ff23e88f1c22d48353ce29e781cdf1
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-01 23:45:47 +0530

    Time: 0 ms (100.00%) | Memory: 30.5 MB (74.00%) - LeetSync

---
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        if(nums.size() <= 1) return 0;
+         int n = nums.size();
+         int left = -1;
+         int right = -1;
+         int maxSeen = nums[0];
+         int minSeen = nums[n-1];
+         // setting the boundary for the right side 
+         // checking if its strictly increasing
+         // for each element first update the maxSeen
+         // since we are checking for global anomaly 
+         // check if its lesser than the maxSeen
+        for(int i = 1 ; i < n ; i++){
+            maxSeen = max(maxSeen,nums[i]);
+            if(maxSeen > nums[i]) right = i;            
+        }
+        // setting the boundary for the left side
+        // checking if its strictly decreasing
+        // for each element update the minSeen
+        // check if the current element is greater than the minSeen
+        for(int j = n - 2 ; j >= 0 ; j--){
+            minSeen = min(minSeen,nums[j]);
+                if(minSeen < nums[j]) left = j;
+        }
+        // since the boundary has been marked 
+        // return the length of the subarray by right - left + 1
+        if(right == -1) return 0;
+        return right - left + 1; 
+    }
+}; 
