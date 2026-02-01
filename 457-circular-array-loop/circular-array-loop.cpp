1bc579649f59ca65cbc8d3de82decf5e2b648ba8
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-01 10:43:20 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (66.91%) - LeetSync

--- 457-circular-array-loop/circular-array-loop.cpp
+++ 457-circular-array-loop/circular-array-loop.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
+    // function to calculate the next
+    // % make sures we are inside a single cycle
     int getNext(int curr,vector<int>&nums){
         int n = nums.size();
         return ((curr + nums[curr]) % n + n ) % n ;
     }
     bool circularArrayLoop(vector<int>& nums) {
         int n = nums.size();
+        // we need to use two loops 
+        // outer loop for starting index 
+        // while loop for cycle detection 
         for(int i = 0 ; i < n ; i++){
             if(nums[i] == 0) continue;
             int slow = i, fast = i;
+            // initial direction 
             int iniDir = nums[i];
             while(true){
+                // before moving slow and fast check its direction
+                // move only if its positive 
                 if(nums[slow] * iniDir <= 0) break;
                 if(nums[fast] * iniDir <= 0) break;
+                // since fast moves two times check the next fast's direction
                 int nextFast = getNext(fast,nums);
                 if(nums[nextFast] * iniDir <= 0) break;
                 slow = getNext(slow,nums);
                 fast = getNext(nextFast,nums);
+                // if slow and fast ever meets
+                // make sure the current index doesn't have a self loop (invalid)
                 if(slow == fast){
                     if(slow == getNext(slow,nums)) break;
                     else return true;
-                }  
+                } 
             }
-            // mark 0 after failure
+            // since we aren't using any extra space (list or something)
+            // make sure to mark the current index as 0 
+            // since we don't need to calculate it again 
              int curr = i;
-                while(nums[curr] == iniDir){
+                while(nums[curr] * iniDir > 0){
                     int next = getNext(curr,nums);
                     nums[curr] = 0 ;
                     curr = next;
