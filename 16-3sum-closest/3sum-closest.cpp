1885c9a8c2c1ef202cc3d4ccc87f8bd8938106e6
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-07 10:42:13 +0530

    Time: 11 ms (61.14%) | Memory: 14 MB (62.91%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,61 +1,23 @@
 class Solution {
-
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-
-        sort(nums.begin(), nums.end());
-
+        sort(nums.begin(),nums.end());
         int n = nums.size();
-
-        long long minDiff = INT_MAX, ans = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
-
+        int ans = 0, minDiff = INT_MAX;
+        for(int i = 0 ; i < n-2 ; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             int left = i + 1, right = n - 1;
-
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == target) {
-
-                    return sum;
-
-                }
-
-                else if (sum > target) {
-
-                    long long diff =(long long)(abs(sum - target)) ;
-
-                    if (diff < minDiff) {
-
-                        ans = sum;
-
-                        minDiff = diff;
-                    }
-
-                    right--;
-
-                }
-
-                else {
-
-                     long long diff = (long long)(abs(sum - target)) ;
-
-                    if (diff < minDiff) {
-
-                        ans = sum;
-
-                        minDiff = diff;
-                    }
-
-                    left++;
-                }
+            while(left < right){
+                 int sum = nums[i]+nums[left]+nums[right];
+                 int currDiff = abs(sum - target);
+                 if(currDiff < minDiff){
+                    minDiff = currDiff;
+                    ans = sum;
+                 }
+                 if(sum < target) left++;
+                 else right--;
             }
         }
-
         return ans;
-    }
+     }
 };
