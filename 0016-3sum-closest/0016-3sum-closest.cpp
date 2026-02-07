c11a113b3293f15b905a047ba16d7d65d97a490d
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-07 10:42:10 +0530

    Time: 11 ms (61.14%), Space: 14 MB (62.91%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -3,28 +3,21 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         int n = nums.size();
-        int minDiff = INT_MAX, ans = 0;
-        for(int i = 0 ; i < n ; i++){
-            if( i > 0 && nums[i] == nums[i-1]) continue;
+        int ans = 0, minDiff = INT_MAX;
+        for(int i = 0 ; i < n-2 ; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             int left = i + 1, right = n - 1;
             while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
-                if(sum == target){
-                    return sum;
-                }
-                int currDiff = abs(sum - target);
-                if(currDiff < minDiff){
-                    ans = sum;
+                 int sum = nums[i]+nums[left]+nums[right];
+                 int currDiff = abs(sum - target);
+                 if(currDiff < minDiff){
                     minDiff = currDiff;
-                }
-                else if(sum > target){
-                     right--;
-                }
-                else{
-                    left++;
-                }
+                    ans = sum;
+                 }
+                 if(sum < target) left++;
+                 else right--;
             }
         }
         return ans;
-    }
+     }
 };
