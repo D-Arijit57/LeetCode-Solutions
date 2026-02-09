a19aa0b72b87f8f090eb999983174fd0ccea53fc
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-09 10:41:53 +0530

    Time: 8 ms (37.24%), Space: 10.8 MB (92.27%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        // instead of simulating the operations we need to check 
+        // if we can maintain a valid window with <= k operations
+        // valid window -> window - maxFreq <= k
+        // We need at most k replacements to make all chars equal.
         int n = s.size();
         vector<int>freq(26,0);
         int left = 0 , maxLen = 0, maxFreq = 0;
         for(int right = 0 ; right < n ; right++){
+            // update the frequency of the character in the window
             freq[s[right] - 'A']++;
+            // update the maxFreq (character with the maximum freq) in the window 
             maxFreq = max(maxFreq,freq[s[right] - 'A']);
+            // while its not a valid window
+            // shrink the window until it becomes valid again 
             while((right - left + 1) - maxFreq > k){
                 freq[s[left] - 'A']--;
                 left++;
             }
+            // update the max length for every valid window
             maxLen = max(maxLen, right - left + 1);
         }
         return maxLen;
