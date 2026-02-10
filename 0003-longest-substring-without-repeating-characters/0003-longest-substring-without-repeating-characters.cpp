b881fa79901ab75e879b8dcc6b6fb077b4fe096a
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-10 10:46:34 +0530

    Time: 0 ms (100%), Space: 11.6 MB (80.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,9 +2,12 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         // since we want the longest substring without repeating characters 
-        // the max frequency inside the window would be <= 1
+        // the maximum frequency for each character inside the window would be <= 1
+        // for that since right is expanding the window 
+        // we will check if the current character right is pointing towards has frequency > 1 or not
         int n = s.size();
         int left = 0, maxLen = 0;
+        // since the strings contain a-z,A-Z,0-9 we will initialize with 256
         vector<int>freq(256,0);
         for(int right = 0 ; right < n ; right++){
             freq[s[right]]++;
@@ -13,7 +16,6 @@ public:
                 left++;
             }
             // for each valid window update the maximum length
-
             maxLen = max(maxLen,  right - left + 1);
         }
         return maxLen;
