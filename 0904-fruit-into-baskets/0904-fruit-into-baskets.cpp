ccad6732761f11a7f5a77d91e13ad896583705f5
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-14 11:44:52 +0530

    Time: 41 ms (62.93%), Space: 84.8 MB (82.34%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -2,18 +2,11 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
-        int left = 0;
-        int maxCnt = 0;
-        // keeping a map to update the frequencies of each type of fruit
         unordered_map<int,int>freq;
+        int left = 0, maxLen = INT_MIN;
         for(int right = 0 ; right < n ; right++){
-            // we use right to expand the window 
-            // for each fruit (new or known) we update the frequency
             freq[fruits[right]]++;
-            // since the basket size is 2 
-            // if there's more than 2 types of fruit
-            // we update the frequency (reduce it)
-            // if any of the fruit's count become zero we remove it
+
             while(freq.size() > 2){
                 freq[fruits[left]]--;
                 if(freq[fruits[left]] == 0){
@@ -21,9 +14,9 @@ public:
                 }
                 left++;
             }
-            // we update the max count for new maximum fruits 
-            maxCnt = max(maxCnt, right - left + 1);
+            
+            maxLen = max(maxLen,right - left + 1);
         }
-        return maxCnt;
+        return maxLen;
     }
 };
