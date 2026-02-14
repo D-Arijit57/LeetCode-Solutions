478a88f152efb21e36f5d3f898668c91298fddeb
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-14 11:44:55 +0530

    Time: 41 ms (62.93%) | Memory: 84.8 MB (82.34%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int, int> basket; // fruit type -> count
-        int left = 0;
-        int maxFruits = 0;
-        
-        for (int right = 0; right < fruits.size(); right++) {
-            // Add current fruit to basket
-            basket[fruits[right]]++;
-            
-            // If we have more than 2 types of fruits, shrink window from left
-            while (basket.size() > 2) {
-                basket[fruits[left]]--;
-                if (basket[fruits[left]] == 0) {
-                    basket.erase(fruits[left]);
+        int n = fruits.size();
+        unordered_map<int,int>freq;
+        int left = 0, maxLen = INT_MIN;
+        for(int right = 0 ; right < n ; right++){
+            freq[fruits[right]]++;
+
+            while(freq.size() > 2){
+                freq[fruits[left]]--;
+                if(freq[fruits[left]] == 0){
+                    freq.erase(fruits[left]);
                 }
                 left++;
             }
             
-            // Update maximum fruits collected
-            maxFruits = max(maxFruits, right - left + 1);
+            maxLen = max(maxLen,right - left + 1);
         }
-        
-        return maxFruits;
+        return maxLen;
     }
 };
