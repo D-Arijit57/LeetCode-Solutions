4a2d3674ca9764ab4e39830f5e516c2431ad9bb4
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-14 10:36:37 +0530

    Time: 0 ms (100.00%) | Memory: 117.9 MB (82.27%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,40 +11,34 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-     if(!head || !head->next) return true;
-     ListNode* slow = head;
-     ListNode* fast = head;
-     // first phase finding the middle of the linked list
-     while(fast && fast->next){
-        slow = slow->next;
-        fast = fast->next->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        // finding the middle
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        // if its a odd position skip it 
-        if(fast != nullptr) slow = slow->next;
-        // since we need to reverse the ll in place we are going to use curr, prev and next
-        // reversing before the middle part
+        // in case if the linkedlist has an odd length 
+        if(fast!=nullptr) slow = slow->next;
         ListNode* curr = slow;
         ListNode* prev = nullptr;
-        // Reversing the second half 
-        while(curr != nullptr){
-            // temporarily storing the next node to be reversed
-            // to access it after reversing the current node 
-            ListNode* next = curr->next;
-            // connect the next pointer to the previous node
-            // prev becomes curr since its has already been reversed 
+        ListNode* next;
+        // reverse the linkedlist
+        while(curr){
+            next = curr->next;
             curr->next = prev;
             prev = curr;
             curr = next;
         }
-        // comparing the first half with the second half 
+        // compare the second half with the first half
         ListNode* first = head;
         ListNode* second = prev;
-        // since the second part actually defines if its a palindrome or not
-        // meaning it would define how many comparisons are supposed to be done 
-        while(second != nullptr){
+        while(second){
             if(first->val != second->val) return false;
-            first = first->next;
-            second = second->next;
+            else {
+                first = first->next;
+                second = second->next;
+            }
         }
         return true;
     }
