bf0861d6ca69c7fc8f5605ec75b4e39fba65d1ad
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-14 11:24:32 +0530

    Time: 20 ms (23.62%), Space: 10.1 MB (98.53%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.cpp
+++ 0457-circular-array-loop/0457-circular-array-loop.cpp
@@ -1,47 +1,43 @@
 class Solution {
 public:
-    int getNext(vector<int>&nums, int curr){
+    // calculate the getNext
+    int getNext(vector<int>&nums,int curr){
         int n = nums.size();
-        return ((curr+nums[curr]) % n + n) % n; 
+        return ((curr+nums[curr]) % n + n ) % n;
     }
     bool circularArrayLoop(vector<int>& nums) {
-        // Loop detection + direction sense       
-        // i -> each node 
-        // nums[i] -> number of steps to jump
         int n = nums.size();
-        for(int i = 0 ; i < n ; i++) {
+        for(int i = 0 ; i < n ; i++){
             if(nums[i] == 0) continue;
-            int dir = nums[i];
             int slow = i, fast = i;
-            // if at any point the direction becomes negative 
-            // since there is no point to check the direction if the loop is going to be invalid
-            // due to opposite direction
-           while(true){
-            // check all at once if one of them has a opposite direction 
-            if(nums[slow] * dir <= 0) break;
-            if(nums[fast] * dir <= 0) break;
-            int nextFast = getNext(nums,fast);
-            if(nums[nextFast] * dir <= 0) break;
-            
-            // move fast and slow 
-            slow = getNext(nums,slow);
-            fast = getNext(nums,getNext(nums,fast));
+            int dir = nums[i];
+            while(true){
+                // check direction before moving slow and fast
+                // direction logic -
+                // if direction is same (both positive / both negative) then the product will be positive
+                if(dir * nums[slow] <= 0) break;
+                slow = getNext(nums,slow);
+                if(dir * nums[fast] <= 0) break;
+                int getNextFast = getNext(nums,fast);
+                if(dir * nums[getNextFast] <= 0) break;
+                fast = getNext(nums,getNextFast);
 
-           // if slow and fast ever meet
-            if(slow == fast) {
-                // check for the self loop first
-                if(slow == getNext(nums,slow)) break;
-                else return true ;
-              }
-           }
-            // marking all the paths as zero which are invalid
-            int curr = i;
-            while(nums[curr] * dir > 0){
-                int next = getNext(nums,curr);
+                // if slow and fast ever meet
+                if(slow == fast){
+                    // check for the self loops
+                    if(slow == getNext(nums,slow)) break;
+                    else return true;
+                }
+            }
+            // marking all the invalid paths as 0
+            // so that we don't check them again
+            // since its  unecessary overhead for re-checking 
+            int curr = i ;
+            while(dir * nums[curr] > 0){
                 nums[curr] = 0;
-                curr = next;
+                curr = getNext(nums,curr);
             }
         }
-    return false ;
+        return false;
     }
 };
