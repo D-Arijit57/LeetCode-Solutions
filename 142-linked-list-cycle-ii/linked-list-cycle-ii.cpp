a229d8d14cd785ad1629707d873829e092546eeb
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-02-23 11:27:43 +0530

    Time: 7 ms (65.85%) | Memory: 11.3 MB (54.41%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,27 +9,21 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(!head || !head->next) return nullptr;
-        // check if there is a cycle in the first place
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast && fast->next){
-            slow = slow -> next;
-            fast = fast->next->next;
-            // if there's any cycle 
-            // find the starting point 
-            if(slow == fast){
-                // reset slow = head (to the starting point)
-                slow = head;
-                // until slow and fast meets again 
-                // move slow and fast by 1 place 
-                while(slow != fast){
-                    slow = slow->next;
-                    fast = fast->next;
-                }
-                return slow;
+     ListNode* slow = head;
+     ListNode* fast = head;
+     while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if(slow == fast){
+            slow = head;
+            while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
             }
+            return slow;
         }
-        return nullptr;
+     }   
+     return nullptr;
     }
 };
