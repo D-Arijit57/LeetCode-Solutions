e6384fb57e12c76e616e4bc11f61268c2a60b354
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-04-02 21:50:32 +0530

    Time: 0 ms (100%), Space: 65.2 MB (25.78%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // floyd's cycle detection + finding the start of the cycle
+        // According to pigeon hole principle if there are n holes and n+1 pigeons
+        // atleast 1 hole will contain more than 1 pigeon
+        // so for sure in this case a duplicate will exist
         int n = nums.size();
-        int slow = 0, fast = 0;
+        int slow = nums[0], fast = nums[0];
+
+        // using floyd's cycle detection 
         while(true){
             slow = nums[slow];
             fast = nums[nums[fast]];
 
+            // if they ever meet, find out the starting point
             if(slow == fast){
-                slow = 0;
+                slow = nums[0];
                 while(slow != fast){
                     slow = nums[slow];
                     fast = nums[fast];
