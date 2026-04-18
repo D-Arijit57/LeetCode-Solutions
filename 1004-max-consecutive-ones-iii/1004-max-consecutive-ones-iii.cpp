b9c1af3a5da7320636d310829827fc1d490a68cb
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-04-19 01:48:21 +0530

    Time: 4 ms (21.28%), Space: 69.6 MB (59.4%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
         int n = nums.size();
-        int zeroCount = 0;
-        int maxLen = 0;
-        int left = 0;
+        // instead of using maps 
+        // focus on the number of zeros coz that's the only thing we need to replace
+        int left = 0 , zeroCount = 0, maxLen = 0;
         for(int right = 0 ; right < n ; right++){
-            if(nums[right] == 0) zeroCount++;
+            if(nums[right] == 0){
+                zeroCount++;
+            }
+            // shrinking logic
             while(zeroCount > k){
-                if(nums[left] == 0) zeroCount--;
+               if(nums[left] == 0) zeroCount--;
                 left++;
             }
-            maxLen = max(maxLen , right - left + 1);
-        }
+            maxLen = max(maxLen, right - left + 1);
+        }   
         return maxLen;
     }
 };
