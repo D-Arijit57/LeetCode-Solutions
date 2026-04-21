9bea74a3ff63175a131a966d61b2e42d32bdcb1a
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-04-22 01:12:13 +0530

    Time: 0 ms (100%), Space: 69.7 MB (26.61%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -2,19 +2,35 @@ class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
         // using maps this time
+        
+        // int n = nums.size();
+        // unordered_map<int, int>freq;
+        // int left = 0, maxLen = 0;
+        // for(int right = 0 ; right < n ; right++){
+        //     freq[nums[right]]++;
+        //     if(nums[right] == 0){
+        //         while(freq[nums[right]] > k){
+        //             freq[nums[left]]--;
+        //             left++;
+        //         }
+        //     }
+        //     maxLen = max(maxLen , right - left + 1);          
+        // }
+        // return maxLen;
+
+        // since we know we only need to count 
+        // and check if the number of zeros > k (to skip invalid window)
+        // we can just keep a counter for the zero
         int n = nums.size();
-        unordered_map<int, int>freq;
-        int left = 0, maxLen = 0;
+        int left = 0, zeroCnt = 0, maxlen = 0;
         for(int right = 0 ; right < n ; right++){
-            freq[nums[right]]++;
-            if(nums[right] == 0){
-                while(freq[nums[right]] > k){
-                    freq[nums[left]]--;
-                    left++;
-                }
+            if(nums[right] == 0) zeroCnt++;
+            while(zeroCnt > k){
+                if(nums[left] == 0) zeroCnt--;
+                left++;
             }
-            maxLen = max(maxLen , right - left + 1);          
+            maxlen = max(maxlen , right - left + 1);
         }
-        return maxLen;
+        return maxlen;
     }
 };
