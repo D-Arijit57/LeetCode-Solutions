ebcc0cba7b8fd67ced421b3e95b61bc2edd53960
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-04-22 22:22:08 +0530

    Time: 0 ms (100%), Space: 32 MB (72.72%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
+        // we shrink it when it is valid to find a smaller window
+        // rather than when it is invalid
         int n = nums.size();
         int left = 0, minLen = INT_MAX, currSum = 0;
         for(int right = 0 ; right < n ; right++){
             currSum += nums[right];
             while(currSum >= target){
+                // We update inside the loop because 
+                // every intermediate shrink still represents a valid window
+                // and we want the smallest among them.
                 minLen = min(minLen, right - left + 1);
                 currSum -= nums[left];
                 left++;
