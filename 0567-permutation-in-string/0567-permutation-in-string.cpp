643d8a6cdc67efa20043f70a8c1791b95a9cbc9d
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-05-06 23:45:02 +0530

    Time: 3 ms (65.35%), Space: 10 MB (44.19%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        if(n > m) return false;
         vector<int>windowFreq(26,0);
         vector<int>targetFreq(26,0);
-        int n = s1.size(), m = s2.size();
-        if(m < n) return false;
-        // setting up the frequency for the s1 
-        for(char c : s1){
-            targetFreq[c - 'a']++;
-        }
-        // setting up the frequency for the first window
-        for(int i = 0 ; i < n ; i++){
+        // update both of the windows
+        for(int i = 0  ; i < n ; i++){
             windowFreq[s2[i] - 'a']++;
+            targetFreq[s1[i] - 'a']++;
         }
         int matches = 0;
+        // check for the first window
         for(int i = 0 ; i < 26 ; i++){
-            if(targetFreq[i] == windowFreq[i]) matches++;
+            if(windowFreq[i] == targetFreq[i]) matches++;
         }
         if(matches == 26) return true;
-        // sliding window approach
+        
+        // checking for the rest of the window
         for(int i = n ; i < m ; i++){
             int right = s2[i] - 'a';
             int left = s2[i - n] - 'a';
 
-            // expanding the window 
-            if(targetFreq[right] == windowFreq[right]) matches--;
+            // update when adding new character
+            if(windowFreq[right] == targetFreq[right]) matches--;
             windowFreq[right]++;
-            if(targetFreq[right] == windowFreq[right]) matches++;
-
-            // shrinking the window
-            if(targetFreq[left] == windowFreq[left]) matches--;
+            if(windowFreq[right] == targetFreq[right]) matches++;
+        
+            // update when removing the old character 
+            if(windowFreq[left] == targetFreq[left]) matches--;
             windowFreq[left]--;
-            if(targetFreq[left] == windowFreq[left]) matches++;
+            if(windowFreq[left] == targetFreq[left]) matches++;
 
-            if(matches == 26) return true;
+            if(matches == 26 ) return true;
         }
         return false;
     }
