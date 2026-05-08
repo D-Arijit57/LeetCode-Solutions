222e092d22ecc8f570b0959cd48d1cd9512fb320
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-05-08 22:54:55 +0530

    Time: 3 ms (24.04%), Space: 27.1 MB (95%) - LeetHub

---
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumSum(vector<int>& arr) {
+        int n = arr.size();
+        int keep = arr[0];
+        // to keep track of the deleted elements
+        // either delete the current one or extend by adding it
+        int deleted = 0;
+        int ans = arr[0];
+        for(int i = 1 ; i < n ; i++){
+            int prevKeep = keep;
+            int prevDeleted = deleted;
+            // at each point
+            // 1. if the element is not beneficial do not include / delete 
+            // 2. if we have already deleted the non beneficial element then include the current one and extend
+            deleted = max(prevKeep, prevDeleted + arr[i]);
+            // conventional kadane's
+            keep = max(arr[i], prevKeep + arr[i]);
+
+            // compare which one is has the maximum sum
+            // since we know the optimal deletion might be no deletion at all 
+            // so we need to compare the conventional kadane's subarray sum as well
+            ans = max({ans, keep, deleted});
+        }
+        return ans;
+    }
+};
