ccfe4b33a010a0cb0b3ddced264a1a9c77e09dbd
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-05 21:24:27 +0530

    Time: 23 ms (54.68%), Space: 17.3 MB (98.81%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,24 +1,22 @@
+#define v vector
+#define ll long long
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
         int n = nums.size();
-        vector<vector<int>>ans;
-        for(int i = 0 ; i < n - 3 ; i++){
+        sort(nums.begin(),nums.end());
+        v<v<int>>ans;
+        for(int i = 0; i < n - 3 ; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
             for(int j = i + 1 ; j < n - 2 ; j++){
                 if(j > i + 1 && nums[j] == nums[j-1]) continue;
                 int left = j + 1, right = n - 1;
                 while(left < right){
-                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
-                    if(sum > target){
-                        right--;
-                    }
-                    else if(sum < target){
-                        left++;
-                    }
-                    else{
-                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+                    ll sum = (ll) nums[i] + nums[j] + nums[left] + nums[right];
+                    if(sum > target) right--;
+                    else if(sum < target) left++;
+                    else {
+                        ans.push_back({nums[i],nums[j],nums[left],nums[right]}); 
                         while(left < right && nums[left] == nums[left + 1]) left++;
                         while(left < right && nums[right] == nums[right - 1]) right--;
                         left++;
