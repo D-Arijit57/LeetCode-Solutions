757f8d2f93f9c5ee6423a99422a7e6d7f16bd496
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-08 03:29:56 +0530

    Time: 3 ms (68.23%) | Memory: 10.8 MB (70.02%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+        //  WHY STALE MAXFREQ DOESN'T MAKE THE ANSWER GO WRONG  
+        //Since we've already observed a frequency of maxFreq, 
+        // we know such a strong character concentration existed at some point. We can safely use that historical best as a certificate.
+        // It may delay shrinking and temporarily keep invalid windows,
+        // but it cannot cause us to miss the optimal answer or report a length that was never achievable.
+
+
+        // It's a valid window if requirement for replacement is less or equal to k 
+        // requirement = windowsize - maxFreq (inside the frequency map)
         int n = s.size();
-        // hasharray to store the frequencies of the uppercase characters
+        // since the string only contains uppercase characters
         vector<int>freq(26,0);
-        int left = 0, maxLen = 0,maxFreq = 0;
+        int left = 0, maxFreq = INT_MIN, maxlen = -1;
         for(int right = 0 ; right < n ; right++){
-            // updating each new entry
             freq[s[right] - 'A']++;
-            // updating the max freq for the current window
-            maxFreq = max(maxFreq, freq[s[right] - 'A']);
-            // shrinking logic 
-            while((right - left + 1)- maxFreq > k){
-                freq[s[left] - 'A']--;
-                left++;
+            // keep updating the max freq after every updation
+            maxFreq = max(maxFreq,freq[s[right] - 'A']);
+            // shrinking logic
+            while((right- left + 1) - maxFreq > k){
+                   freq[s[left] - 'A']--;
+                   left++;             
             }
-            // updating the maxlength 
-            maxLen = max(maxLen , right - left + 1);
-            }
-            return maxLen;
+            maxlen = max(maxlen, right - left + 1);          
         }
+        return maxlen == -1 ? -1 : maxlen;
+    }
 };
