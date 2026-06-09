1e6573a0c923e08eefc2ec0c78bae1f64757918b
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-09 21:21:19 +0530

    Time: 12 ms (43.65%) | Memory: 35.6 MB (48.59%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int n = nums.size();
+        unordered_map<int, int>mp;
+        int cnt = 0 ;
+        // empty check point , subarray can start from the 0th index 
+        mp[0] = 1;
+        int remainder = 0, prefix = 0;
+        for(int i = 0 ; i < n ; i++){
+            // keep updating the running sum 
+            prefix += nums[i];
+
+            // normalization to tackle the negative numbers
+            remainder = ((prefix % k) + k) % k;
+
+            // if the remainder exists then add up its frequency to the count
+                if(mp.count(remainder)){
+                 cnt += mp[remainder];
+                }
+
+            // if it doesn't exist then store the remainder
+            mp[remainder]++;
+         }
+        return cnt;
+        }
+};
