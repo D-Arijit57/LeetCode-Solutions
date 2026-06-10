04edc4aa9367f35afc63f793194d7fc4aeda7dc8
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-11 04:07:33 +0530

    Time: 84 ms (21.41%) | Memory: 146.2 MB (80.96%) - LeetSync

---
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        // since we need to keep the track of the subarray length 
+        // we need to store indices instead of frequencies
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        mp[0] = -1; // before the array starts 
+        int prefix = 0, rem = 0;
+        for (int i = 0; i < n; i++) {
+            prefix += nums[i];
+            // calculate the remainder
+            rem = prefix % k;
+
+            // if you already found somewhere means the multiple exist
+            if (mp.count(rem)) {
+                // check the sub-array length if its atleast 2
+                int prevInd = mp[rem]; // gives the index it occured at first
+                if (i - prevInd >= 2)
+                    return true;
+            }
+            // update the remainder index
+            else
+                mp[rem] = i;
+        }
+        return false;
+    }
+};
