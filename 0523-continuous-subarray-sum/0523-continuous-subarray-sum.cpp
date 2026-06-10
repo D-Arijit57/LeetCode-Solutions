2cbd723a029711d68fc987c59a6a22360e1dec1f
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-11 04:07:13 +0530

    Time: 79 ms (51.99%), Space: 146.3 MB (53.74%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,24 +1,29 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-      int n = nums.size();
-      unordered_map<int, int>mp;
-      mp[0] = -1; 
-      int prefix = 0, rem = 0;
-      for(int i = 0 ; i < n ; i++){
-        prefix += nums[i];
-        // calculate the remainder
-        rem = prefix % k;
+        // since we need to keep the track of the subarray length 
+        // we need to store indices instead of frequencies
 
-        // if you already found somewhere means the multiple exist
-        if(mp.count(rem)) {
-            // check the sub-array length if its atleast 2 
-            int prevInd = mp[rem]; // gives the index it occured at first 
-            if(i - prevInd >= 2) return true;
+        int n = nums.size();
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        int prefix = 0, rem = 0;
+        for (int i = 0; i < n; i++) {
+            prefix += nums[i];
+            // calculate the remainder
+            rem = prefix % k;
+
+            // if you already found somewhere means the multiple exist
+            if (mp.count(rem)) {
+                // check the sub-array length if its atleast 2
+                int prevInd = mp[rem]; // gives the index it occured at first
+                if (i - prevInd >= 2)
+                    return true;
+            }
+            // update the remainder index
+            else
+                mp[rem] = i;
         }
-        // update the remainder frequency
-       else mp[rem] = i;
-      }
-      return false;
+        return false;
     }
 };
