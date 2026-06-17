c7784d5578c660924dbff475f7bb5b557dde1a62
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-17 20:49:45 +0530

    Time: 3 ms (77.21%) | Memory: 53.1 MB (58.20%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -16,6 +16,10 @@ public:
         // if not then that index has the incorrect number
         for(int i = 0 ; i < n ; i++){
             if( i != nums[i] - 1){
+                // for a range 1 to n, the value x should be at x-1
+                // so the index i should hold the value i+1
+                // since the index i is occupied by some other value
+                // i+1 is the number that is missing from the array 
                 ans.push_back(i+1);
             }
         }
