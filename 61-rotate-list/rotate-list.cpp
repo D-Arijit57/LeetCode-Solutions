3017a6606829843ca7007bed5b238d3e7c21f0e6
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-20 03:33:31 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (31.20%) - LeetSync

---
+++ 61-rotate-list/rotate-list.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next || k == 0) return head;
+        // if we observe this is not a reversal problem 
+        // what we really need to do here is 
+        // we need to find a split point, cut the second half, reconnect it to the head 
+        // if we observe the position of the new head is : n - k + 1 , new tail : n - k( n: the size of the linkelist )
+        ListNode* temp = head;
+        int n = 1;
+        while(temp->next){
+            temp = temp->next;
+            n++;
+        }
+        // make the list circular
+        temp->next = head;
+        // find the new tail 
+        // reduce k (normlization)
+        k %= n;
+        int newHead = n - k + 1;
+        int newTail = n - k;
+        ListNode* nh = head;
+        ListNode* nt = head;
+        // find the newtail
+        for(int i = 1 ; i < newTail ; i++){
+            nt = nt->next;
+        }
+        for(int i = 1; i < newHead ; i++){
+            nh = nh->next;
+        }
+        nt->next = nullptr;
+        return nh;
+    }
+};
