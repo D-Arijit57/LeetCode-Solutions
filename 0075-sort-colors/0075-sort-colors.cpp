81f09df8b8f1d4fc7711f77ce45bfcad00e46845
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-21 01:28:42 +0530

    Time: 0 ms (100%), Space: 11.5 MB (78.16%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,27 +1,32 @@
+// Rev 20th June : 
+// Mistakes : 
+// 1. Forgot the invariant, which regions defines what which cause the confused pointer movements 
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-              // dutch national flag algorithm
-              // mid == 1 keep moving forward
-              // mid == 0 swap with left 
-              // mid == 2 swap with right 
-              int n = nums.size();
-              if(n <= 1) return;
-              int left = 0, right = n - 1;
-              int mid = left;
-              while(mid <= right){
-                if(nums[mid] == 1) mid++;
-                else if(nums[mid] == 0){
-                    swap(nums[mid],nums[left]);
-                    left++;
-                    mid++;
-                }
-                else{
-                    swap(nums[mid],nums[right]);
-                    right--;
-                    // since we don't know what we got from the right
-                    // right after swapping we shouldn't move the mid yet
-                }
-            } 
+        int n = nums.size();
+        int left = 0 , right = n - 1;
+        int mid = left;
+        while(mid <= right){
+            // if the color is white do nothing move forward since 1 should be in the middle
+            if(nums[mid] == 1) mid++;
+            // if the color is red then swap it up with the left, since it should be at the left
+            // according to the invariant we know that from left...mid-1 we got only 1s
+            // so the element swapped with left is 1 only so we can safely move left and mid together
+            else if(nums[mid] == 0){
+                swap(nums[left], nums[mid]);
+                left++;
+                mid++;
+            }
+            // if the color is blue then swap it up with the right, since it should be at the right
+            // according to the invariant mid-1...right is unknown
+            // so we don't what got from the right when we swapped so we must check that before moving both mid and right
+            else{
+                swap(nums[right], nums[mid]);
+                right--;
+            }
+         
+        }
+        
     }
 };
