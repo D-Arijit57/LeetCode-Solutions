048eb33fecb1423be28b3db6eab85a52677ee090
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-21 02:03:40 +0530

    Time: 0 ms (100%), Space: 30.4 MB (73.23%) - LeetHub

--- 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        int n = nums.size();
-        int maxSeen = nums[0], minSeen = nums[n-1];
-        int left = 0, right = 0;
-        // Moving forward to set the boundary for the right
-        for(int i = 0 ; i < n ; i++){
-            // A Smaller element shouldn't be in the right side 
-            maxSeen = max(maxSeen, nums[i]);
-            if(nums[i] < maxSeen){
-                left = i;
+        
+            int n = nums.size();
+            int left = -1, right = -1;
+            int maxSeen = nums[0],  minSeen = nums[n-1];
+            for(int i = 0; i < n ; i++){
+                // update the global max
+                // any small element shouldn't be in the right in the sorted array 
+                maxSeen = max(maxSeen, nums[i]);
+                if(nums[i] < maxSeen){
+                    right = i;
+                }
             }
-        }
-        // Moving backward to set the boundary for the left 
-        for(int i = n - 1; i >= 0 ; i--){
-            // A larger element shouldn't be on the left side 
-            minSeen = min(minSeen, nums[i]);
-            if(nums[i] > minSeen){
-                right = i;
+            for(int i = n-1 ; i >= 0; i--){
+                // update the global min 
+                // any greater element shouldn't be in the left in the sorted array 
+                minSeen = min(minSeen,nums[i]);
+                if(nums[i] > minSeen){
+                    left = i;
+                }
             }
+            // if one of them doesn't move then the array is already sorted
+            // check if left moved or not
+            return left == -1 ? 0 : right - left + 1;
         }
-        return left == 0 ? 0 : left - right + 1;
-    }
 };
