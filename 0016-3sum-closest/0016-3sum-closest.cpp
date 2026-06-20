93203d4ada9fe29d7a79dcd83ea5d08137c63f17
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-21 00:59:20 +0530

    Time: 15 ms (31.21%), Space: 14 MB (36.85%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        // before using two pointers we need to make sure the array is sorted
         sort(nums.begin(),nums.end());
         int n = nums.size();
-        int minDiff = INT_MAX, ans = 0;
+        int ans = 0, minDiff = INT_MAX;
         for(int i = 0 ; i < n ; i++){
-            if(i > 0 && nums[i] == nums[i -1]) continue;
+            // skip the same triplets
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             int left = i + 1, right = n - 1;
+            // find the sum 
+            // if sum == target then return
+            // otherwise update the difference between the target and the sum
+            // the minimum the distance the more closer the sum is 
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
-                // MISTAKE 1. Forgot to use the absolute difference
                 int diff = abs(target - sum);
+
+                // once you find a new minimum distance update the answer as well as the minDiff seen till now 
                 if(diff < minDiff){
-                    minDiff = min(diff, minDiff);
+                    minDiff = min(minDiff, diff);
                     ans = sum;
-                    }
+                }
+                
                 if(sum == target) return sum;
-                else if(sum < target) {
-                    left++;
-                }
-                else{
-                    right--;
-                }
+                else if(sum < target) left++;
+                else right--;
             }
-         }
-         return ans;
+        }
+        return ans;
     }
 };
