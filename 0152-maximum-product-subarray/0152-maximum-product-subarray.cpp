28a3c60f0e255499d64b7f81e5669a4fedb81738
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-27 22:25:57 +0530

    Time: 4 ms (11.23%), Space: 17.7 MB (86.36%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         // today's minimum could become tomorrow's maximum
-        // since negative * negative = positive 
+        // since negative * negative = positive
         // so keep the track of currMin
         // maximum product could be negative as well
         // so we have to consider both the positive and negative scenario
         int n = nums.size();
         int currMax = nums[0], currMin = nums[0];
         int maxProd = nums[0];
-        for(int i = 1 ; i < n ; i++){
+        for (int i = 1; i < n; i++) {
+            // keep the track
             // store the current values right now
             // tomorrow it could create the new maximum product
             int oldMax = currMax;
             int oldMin = currMin;
 
-            // updating the new 
+            // Three possibilities for the maximum/minimum product ending at
+            // nums[i]:
+            // 1. Start a new subarray from nums[i]
+            // 2. Extend the previous maximum product
+            // 3. Extend the previous minimum product
             currMax = max({nums[i], oldMax * nums[i], oldMin * nums[i]});
             // we're keeping the track of the minimum
             // since it can contribute to tomorrow's maximum
