ec88ceff1be37afeb3b0f065f0bfd489c6fad56f
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-27 22:23:08 +0530

    Time: 0 ms (100%), Space: 17.7 MB (59.3%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        // today's minimum could become tomorrow's maximum
+        // since negative * negative = positive 
+        // so keep the track of currMin
+        // maximum product could be negative as well
+        // so we have to consider both the positive and negative scenario
         int n = nums.size();
-        int currMax = nums[0], currMin = nums[0], ans = nums[0];
-        for(int i = 1; i < n ; i++){
-            int prevMax = currMax;
-            int prevMin = currMin;
+        int currMax = nums[0], currMin = nums[0];
+        int maxProd = nums[0];
+        for(int i = 1 ; i < n ; i++){
+            // store the current values right now
+            // tomorrow it could create the new maximum product
+            int oldMax = currMax;
+            int oldMin = currMin;
 
-            currMax = max({nums[i], prevMax * nums[i], prevMin * nums[i]});
-            currMin = min({nums[i], prevMax * nums[i], prevMin * nums[i]});
+            // updating the new 
+            currMax = max({nums[i], oldMax * nums[i], oldMin * nums[i]});
+            // we're keeping the track of the minimum
+            // since it can contribute to tomorrow's maximum
+            currMin = min({nums[i], oldMax * nums[i], oldMin * nums[i]});
 
-            ans = max(currMax, ans);
+            // updating the maximum product
+            maxProd = max(maxProd, currMax);
         }
-        return ans;
-        
+        return maxProd;
     }
 };
