e3980d9fe6ba38332b8fd6838869e4ed1f9ca215
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-27 22:26:01 +0530

    Time: 4 ms (11.23%) | Memory: 17.7 MB (86.36%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        // today's minimum could become tomorrow's maximum
+        // since negative * negative = positive
+        // so keep the track of currMin
+        // maximum product could be negative as well
+        // so we have to consider both the positive and negative scenario
         int n = nums.size();
-        int currMax = nums[0], currMin = nums[0], ans = nums[0];
-        for(int i = 1; i < n ; i++){
-            int prevMax = currMax;
-            int prevMin = currMin;
+        int currMax = nums[0], currMin = nums[0];
+        int maxProd = nums[0];
+        for (int i = 1; i < n; i++) {
+            // keep the track
+            // store the current values right now
+            // tomorrow it could create the new maximum product
+            int oldMax = currMax;
+            int oldMin = currMin;
 
-            currMax = max({nums[i], prevMax * nums[i], prevMin * nums[i]});
-            currMin = min({nums[i], prevMax * nums[i], prevMin * nums[i]});
+            // Three possibilities for the maximum/minimum product ending at
+            // nums[i]:
+            // 1. Start a new subarray from nums[i]
+            // 2. Extend the previous maximum product
+            // 3. Extend the previous minimum product
+            currMax = max({nums[i], oldMax * nums[i], oldMin * nums[i]});
+            // we're keeping the track of the minimum
+            // since it can contribute to tomorrow's maximum
+            currMin = min({nums[i], oldMax * nums[i], oldMin * nums[i]});
 
-            ans = max(currMax, ans);
+            // updating the maximum product
+            maxProd = max(maxProd, currMax);
         }
-        return ans;
-        
+        return maxProd;
     }
 };
