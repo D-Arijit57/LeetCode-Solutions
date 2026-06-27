96da10a58086e3b0318e0026abff91eced18638a
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-27 20:41:22 +0530

    Time: 46 ms (46.38%) | Memory: 84.9 MB (62.29%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
-        int maxi = INT_MIN, left = 0;
-        unordered_map<int,int>freq;
+        unordered_map<int, int>freq;
+        int left = 0 ;
+        int maxFruits = INT_MIN;
         for(int right = 0 ; right < n ; right++){
             freq[fruits[right]]++;
+
+            // shrink the if the window is invalid
             while(freq.size() > 2){
                 freq[fruits[left]]--;
-                if(freq[fruits[left]] == 0) freq.erase(fruits[left]);
+                // if the count becomes zero
+                // remove it from the map
+                if(freq[fruits[left]] == 0 ) freq.erase(fruits[left]);
                 left++;
             }
-            maxi = max(maxi, right - left + 1);
+            maxFruits = max(maxFruits, right - left + 1);
         }
-        return maxi;
+    return maxFruits;
     }
 };
