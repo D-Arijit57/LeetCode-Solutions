4d835139d72a268bee0633d0ee308b09de148da3
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-28 19:55:01 +0530

    Time: 49 ms (50.61%), Space: 29 MB (73.14%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // make sure to skip duplicates by checking neighbours
+        // the same element we have seen before can contribute to a duplicate triplet
+        sort(nums.begin(),nums.end());
         int n = nums.size();
         vector<vector<int>>ans;
-        // since we are checking triplets it makes no sense to iterate till n
-        // we can just iterate till n - 2 positions    
-        for(int i = 0 ; i < n-2 ; i++){
-            // skipping duplicates since it would lead to duplicate triplets
-            if(i > 0 && nums[i - 1] == nums[i]) continue;
-            int left = i + 1 , right = n - 1;
+        for(int i = 0 ; i < n - 2 ; i++){
+            // skip duplicates
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i+1, right = n-1;
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
-                if(sum > 0) right--;
-                else if(sum < 0) left++; 
-                else { // when sum is equal to the target
+                if(sum < 0) left++;
+                else if(sum > 0) right--;
+                else{
                     ans.push_back({nums[i],nums[left],nums[right]});
-                    // skipping the duplicates
-                    while(left < right && nums[left] == nums[left + 1]) left++;
-                    while(left < right && nums[right] == nums[right - 1]) right--;
+                    // skip duplicates 
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
                     left++;
                     right--;
-                }
+                } 
             }
-        }
+        } 
         return ans;
     }
 };
