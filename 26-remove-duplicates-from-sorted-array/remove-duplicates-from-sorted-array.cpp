d36605323b34aef4056f9a8aa2dfd227938a370d
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-28 19:39:28 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (52.51%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        // use left as write pointer and right as a read pointer
+        // since every element should have only one instance till k
+        // we can for sure say Left will move by one place after swapping
         int n = nums.size();
         int left = 0, right = 0;
         for(int i = 0 ; i < n ; i++){
