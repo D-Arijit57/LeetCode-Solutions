e66b5c42a6c57f4586f0881a2299cbf2cb03498a
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-06-28 19:28:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (64.52%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+        // since the array is sorted
+        // we can use two pointers
+        // the more we go towards right the larger elements we get 
+        // and smaller elements if we go left
+        // that way we can adjust the sum according to the target 
+        // to find the answer indices
         int n = numbers.size();
         int left = 0, right = n - 1;
         while(left < right){
