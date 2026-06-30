e92ad2f7697ac271e6eed5582cff37935dfdfbfb
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-01 00:30:26 +0530

    Time: 7 ms (48.46%), Space: 14.3 MB (45.31%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        // we need to remove two adjacent and equal letters 
-        // till s no longer consists the duplicate characters
-        // we need to pop the element when it matches with the top
-        // because it is the duplicate of the element we last saw
-        // The stack will always represent the string after removing all adjacent duplicates processed so far.
-        stack<char>st;
+        // The stack stores the current string after all removals.
+        // If the current character matches the last kept character,
+        // they form an adjacent duplicate pair, so remove the top.
+        stack<char> st;
         string ans = "";
-        for(char c : s){
+        for (char c : s) {
             // Remove exactly one adjacent pair.
             // do won't use a while it'll keep popping
             // we are doing it for a pair so. a single 'if' is enough
-            if(!st.empty() && st.top() == c){
+            if (!st.empty() && st.top() == c) {
                 st.pop();
             }
             // otherwise push the character
-            else st.push(c);
+            else
+                st.push(c);
         }
         // build the string from the stack
-        while(!st.empty()){
-           ans += st.top();
-           st.pop();
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
         }
         // since its reverse order because of stack
         // reverse it again
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
