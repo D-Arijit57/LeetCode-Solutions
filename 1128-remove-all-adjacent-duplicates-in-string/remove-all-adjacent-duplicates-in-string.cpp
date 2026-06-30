ce6d14baad211b11478a5a6c4c46af67b502dcc6
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-01 00:30:28 +0530

    Time: 7 ms (48.46%) | Memory: 14.3 MB (45.31%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
-        // we need to remove two adjacent and equal letters 
-        // till s no longer consists the duplicate characters
-        // we need to pop the element when it matches with the top
-        // because it is the duplicate of the element we last saw
-        // The stack will always represent the string after removing all adjacent duplicates processed so far.
-        int n = s.size();
-        stack<char>st;
-        st.push(s[0]);
+        // The stack stores the current string after all removals.
+        // If the current character matches the last kept character,
+        // they form an adjacent duplicate pair, so remove the top.
+        stack<char> st;
         string ans = "";
-        for(int i = 1 ; i < n ; i++){
+        for (char c : s) {
             // Remove exactly one adjacent pair.
             // do won't use a while it'll keep popping
             // we are doing it for a pair so. a single 'if' is enough
-            if(!st.empty() && st.top() == s[i]){
+            if (!st.empty() && st.top() == c) {
                 st.pop();
             }
             // otherwise push the character
-            else st.push(s[i]);
+            else
+                st.push(c);
         }
         // build the string from the stack
-        while(!st.empty()){
-           ans += st.top();
-           st.pop();
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
         }
         // since its reverse order because of stack
         // reverse it again
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
