173321345d1399b1dbd199d0e5d222ef634d3a8f
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-01 00:26:37 +0530

    Time: 4 ms (68.17%), Space: 14.3 MB (31.84%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
+        // we need to remove two adjacent and equal letters 
+        // till s no longer consists the duplicate characters
         int n = s.size();
         stack<char>st;
         st.push(s[0]);
@@ -8,7 +10,7 @@ public:
         for(int i = 1 ; i < n ; i++){
             // Remove exactly one adjacent pair.
             // do not use a while it'll keep popping
-            // we are doing it for a pair so, 'if' is enough
+            // we are doing it for a pair so. a single 'if' is enough
             if(!st.empty() && st.top() == s[i]){
                 st.pop();
             }
