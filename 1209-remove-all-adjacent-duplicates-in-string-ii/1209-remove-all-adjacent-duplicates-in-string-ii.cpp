69e40c2ae930b201eab5d9e76fa41fd3bf885223
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-01 22:27:46 +0530

    Time: 17 ms (18.61%), Space: 14.2 MB (34.28%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        // each incoming character either extends the previous group or starts a new one
-        if(s.size() <= 1) return s;
-        // unlike an adjacent pair we need to store consecutive groups of each character
+        // each incoming character either extends the previous group or stars a new one
+        // unlike an adjacent pair we need to stroe consecutive groups of each character
         // to track how many times each character appeared in the string
         // stack will store the current reduced string with the count of the consecutive group at the end
         int n = s.size();
         string ans = "";
-        // stack -> (character, frequency)
-        stack<pair<char,int>>st;
-        st.push({s[0],1});
+        // stack -> (character, frequency)-> groups
+        stack<pair<char,int>>groups;
+        groups.push({s[0],1});
         for(int i = 1 ; i < n ; i++){
             // that is the top of the stack becomes equal with the current element
             // do not use while since it would update the frequency multiple times since the character will be same because of the condition
-            if(!st.empty() && st.top().first == s[i]){
+            if(!groups.empty() && groups.top().first == s[i]){
                // increase the frequency
-               // update the frequency first since we have found it again
-                st.top().second++;
+               // update the frequency firgroups since we have found it again
+                groups.top().second++;
 
                // if the count is exactly equal to k
                // pop the element
-               if(st.top().second == k){
-                st.pop();
+               if(groups.top().second == k){
+                groups.pop();
                }
                
             }
             // if the incoming character doesn't extend the previous group
             // push the new group
-            else st.push({s[i],1});
+            else groups.push({s[i],1});
         }
-        // rebuild the string
-        while(!st.empty()){
+        // rebuild the groupsring
+        while(!groups.empty()){
             // make sure you remember the frequency of each character
             // do not just add them once
-            ans.append(st.top().second, st.top().first);
-            st.pop();
+            ans.append(groups.top().second, groups.top().first);
+            groups.pop();
         }
         // reverse since the order is reversed in the stack
         reverse(ans.begin(),ans.end());
