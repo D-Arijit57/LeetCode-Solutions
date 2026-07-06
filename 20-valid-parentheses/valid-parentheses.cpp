e20a39903cab8a9050fe2dd7306c0852c7dfc538
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-06 11:22:14 -0700

    Time: 0 ms (100.00%) | Memory: 9 MB (18.56%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
+        // each incoming character can be a pair of the top or start of a new
+        // pair at the end if its valid then stack will be empty for each
+        // correct pair pop the top otherwise push it
+        unordered_map<char, char> pairs = {
+            {'(', ')'},
+            {'{', '}'},
+            {'[', ']'}
+        };
         stack<char> st;
-        unordered_map<char, char> CloseToOpen = {
-            {')', '('}, {'}', '{'}, {']', '['}};
-        for (char c : s) {
-            if (CloseToOpen.count(c)) {
-                if (!st.empty() && st.top() == CloseToOpen[c]) {
-                    st.pop();
-                } else
-                    return false;
-            } else {
-                st.push(c);
+        for (int i = 0; i < s.size(); i++) {
+            // each incoming element can only cancel out a single element
+            // if its the pair of the top then pop it
+            if (!st.empty() && s[i] == pairs[st.top()]) {
+                st.pop();
             }
+            // if its not a pair of the top then push it
+            else st.push(s[i]);
         }
         return st.empty();
     }
-    };
+};
