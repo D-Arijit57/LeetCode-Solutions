9184502d74297beecea7fd513c43067d0b572ea0
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-07 09:37:20 -0700

    Time: 20 ms (19.85%) | Memory: 14.8 MB (38.86%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        // non-repeating character means frequency is 1
+        // store the frequency
+        // unordered_map does not preserve the order of the characters,
+        // so we iterate over the original string to find the first character
+        // whose frequency is 1.
+        int n = s.size();
+        unordered_map<char, int> freq;
+        // store the frequency
+        // leetcode {key :  val} -> {character , frequency}
+        // {l : 1}
+        for (char c : s) {
+            freq[c]++;
+        }
+        // re-iterate the map
+        // starting from the left and find the character with frequency == 1
+        // since we are using a unordered_map, iterating over the map won't work
+        // so we'll iterate over the string from left to right and compare each
+        // character's frequency
+        for (int i = 0; i < n; i++) {
+            if (freq[s[i]] == 1)
+                return i;
+        }
+        // if there isn't any non repeating character
+        // then return -1
+        return -1;
+    }
+};
