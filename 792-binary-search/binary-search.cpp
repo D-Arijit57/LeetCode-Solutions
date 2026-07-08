67cc6c03e2be9892c8d4a2b053679fd2624027dd
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-08 14:17:44 -0700

    Time: 3 ms (1.69%) | Memory: 31.3 MB (81.49%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
         // eliminate the search space
         int n = nums.size();
         int low = 0 , high = n - 1;
