1e2a7759b48dd8372858452ce12d1341090d11f6
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-08 09:35:34 -0700

    Time: 3 ms (53.77%) | Memory: 12.1 MB (11.40%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        if(magazine.size() < ransomNote.size()) return false;
+        // update the frequencies for ransomeNote and magazine
+        // the character in the ransomNote should have a frequency
+        // character_in_ransomNote >= character_in_magazine
+        unordered_map<char,int>ransomNoteFreq;
+        unordered_map<char,int>magazineFreq;
+        // update the frequency of characters from the ransomNote 
+        for(char c : ransomNote){
+            ransomNoteFreq[c]++;
+        }
+
+        //update the frequency of characters from the magazine
+        for(char c : magazine){
+            magazineFreq[c]++;
+        }
+
+        for(const auto& it : ransomNoteFreq){
+            // check if the count is sufficient for the ransomNote
+            if(magazineFreq[it.first] < ransomNoteFreq[it.first]) return false;
+        }
+        return true;
+    }
+};
