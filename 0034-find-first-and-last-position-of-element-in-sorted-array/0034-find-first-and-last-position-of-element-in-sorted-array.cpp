c22127b8dd8da5944677d6e594f4fbaeb236bfdb
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-10 08:58:10 -0700

    Time: 0 ms (100%), Space: 17.7 MB (19.6%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
- // implementing lowerbound and upperbound
-int lowerbound(vector<int>&nums , int target){
-    int n = nums.size();
-    int ans = n , low = 0, high = n-1;
-    while(low<=high){
-        int mid = (low+high) / 2;
-        if(nums[mid] >= target){
-            ans = mid;
-            high = mid - 1;
-        }
-        else low = mid + 1;
-    }
-    return ans;
-}
-int upperbound(vector<int>&nums , int target){
-    int n = nums.size();
-    int ans = n , low = 0, high = n-1;
-    while(low<=high){
-        int mid = (low+high) / 2;
-        if(nums[mid] > target){
-            ans = mid;
-            high = mid - 1;
-        }
-        else low = mid + 1;
-    }
-    return ans;
-}
     vector<int> searchRange(vector<int>& nums, int target) {
-     int lb = lowerbound(nums,target);
-     int ub = upperbound(nums,target);
-     int n = nums.size();
-     if(lb == n|| nums[lb] != target) return {-1,-1};
-     return {lb , ub-1};
+        // edge case if the array is empty return {-1,-1}
+        if (nums.size() == 0)
+            return {-1, -1};
+        // if the element at the mid is greater than the target eliminate the
+        // right half eliminate the left half otherwise we need to find the
+        // valid the rightmost and leftmost target so whenever you find an mid
+        // with a value of >= target, you want to find the left most valid
+        // position of it so you do high = mid - 1; otherwise you do the same
+        // for left whenever it is with a value of <= target
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        vector<int> ans(2, -1);
+        // binary search 1 to find the left most valid occurence
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] > target) high = mid - 1;
+            else if(nums[mid] < target ) low = mid + 1;
+            // whenever its equal to the target
+            // update the ans and move towards left to find the left most valid occurence 
+            else{
+                ans[0] = mid;
+                high = mid - 1;
+            } 
+        }
+        low = 0, high = n - 1;
+        // binary search 2 to find the right most valid occurence 
+        while(low <= high){
+            int mid = low  + (high - low) / 2;
+            if(nums[mid] < target) low = mid + 1;
+            else if(nums[mid] > target) high = mid - 1;    
+            // if nums[mid] == target then update the end
+            // move towards the right to find the right most valid occurence
+            else{
+                ans[1] = mid;
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
+    
 };
