eb006318e6d85371948cf7e34445c42345f591b6
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-12 22:35:51 -0700

    Time: 0 ms (100.00%) | Memory: 21.7 MB (76.33%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& intervals,
-                               vector<int>& newInterval) {
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        // since the intervals array is sorted
+        // there are three cases for the current intervals
+        // 1. it starts before the new interval
+        // 2. it overlaps with the new interval
+        // 3. it starts after the new inteval
         int n = intervals.size();
-        vector<vector<int>> ans;
-        int i = 0;
-        // Phase 1:  push everything before the new interval
-        while (i < n && intervals[i][1] < newInterval[0]) {
-            ans.push_back(intervals[i]);
-            i++;
+        vector<vector<int>>ans;
+        for(int i = 0 ; i < n ; i++){
+            // case 1.  it starts before the new interval
+            if(intervals[i][1] < newInterval[0]){
+                // push it in the answer array
+                ans.push_back(intervals[i]);
+            }
+            // case 2. its a overlapping interval
+            // merge it 
+            else if(intervals[i][0] <= newInterval[1]){
+                // take the minimum start and the maximum end comparatively to the current interval
+                // and update the new interval
+                newInterval[0] = min(intervals[i][0], newInterval[0]);
+                newInterval[1] = max(intervals[i][1], newInterval[1]);
+            }
+            // if the newInterval now doesn't overlap with anyone push it in the answer array
+            else{
+                ans.push_back(newInterval);
+                // current interval becomes the new interval 
+                newInterval = intervals[i];
+            } 
         }
-        // Phase 2 : push overlaps
-        while (i < n && intervals[i][0] <= newInterval[1]) {
-            newInterval[0] = min(newInterval[0], intervals[i][0]);
-            newInterval[1] = max(newInterval[1], intervals[i][1]);
-            i++;
-        }
+        // push the last remaining interval 
         ans.push_back(newInterval);
-        // Phase 3 : push everything after the new interval
-        while (i < n) {
-            ans.push_back(intervals[i]);
-            i++;
-        }
         return ans;
-        }
+    }
 };
