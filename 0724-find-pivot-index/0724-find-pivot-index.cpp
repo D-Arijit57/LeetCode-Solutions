ddae355581fbac131d51dc75a419403d9ec6babc
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-12 21:25:07 -0700

    Time: 0 ms (100%), Space: 36.4 MB (17.67%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-       int n = nums.size();
-       vector<int>prefix(n+1,0);
-       // to store the values in the prefix array 
-       for(int i = 0; i < n ; i++){
+        // for an index to be pivot index
+        // at index i, currPrefix - oldPrefix = 0
+        // since the same is same
+        int n = nums.size();
+        vector<int>prefix(n+1,0);
+        // store the prefix sum
+        for(int i = 0 ; i < n ; i++){
             prefix[i+1] = prefix[i] + nums[i];
-       } 
-       // now at each index check if left sum == right sum
-       int left_sum = 0, right_sum = 0;
-       for(int i = 0 ; i < n; i++){
-            left_sum = prefix[i];
-            right_sum = prefix[n] - prefix[i+1];
+        }
+        
+        int left_sum = 0, right_sum = 0;
+        for(int i = 0 ; i < n ; i++){
+            // left sum will be prefix[i] 
+            left_sum = prefix[i]; 
+            // right sum will be total - prefix[i + 1]  
+            right_sum =  prefix[n] - prefix[i + 1];
             if(left_sum == right_sum) return i;
-       }
-       return -1;
+        }
+        return -1;
     }
 };
