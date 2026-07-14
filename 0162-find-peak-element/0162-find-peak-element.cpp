4882b1966e17c7ee81c1ee14e715c2da74165309
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-14 10:30:34 -0700

    Time: 0 ms (100%), Space: 12.6 MB (72.25%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,26 +1,37 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-    // the elimination of either of the halves depends on the mid
-    // if nums[mid] < nums[mid + 1] we can see there is an increasing slope towards right
-    // because of this there are two possibilites
-    //1. if the slope keeps increasing to the very end, the last element becomes a peak
-    // because we know nums[-1] = nums[n] = inifinity (i.e first and last element are striclty greater than elements outside of the array)
-    // 2. the slope eventually steeps down somewhere, the point before the slope steeps down is where we could say that a peak exists
-    int n = nums.size();
-    int low = 0 , high = n - 1;
-    while(low < high){
-        int mid = low + (high - low) / 2;
-        // there is an increasing trend / slope towards right
-        if(nums[mid] < nums[mid + 1]){
-            low = mid + 1;
+        // for this problem we aren't supposed to justify a peak's identity
+        // but a peak's existence
+        
+        // the elimination of either of the halves depends on the mid
+        // if nums[mid] < nums[mid + 1] we can see there is an increasing slope
+        // towards right because of this there are two possibilites
+        // 1. if the slope keeps increasing to the very end, the last element
+        // becomes a peak
+        // because we know nums[-1] = nums[n] = inifinity (i.e first and last
+        // element are striclty greater than elements outside of the array)
+        // 2. the slope eventually steeps down somewhere, the point before the
+        // slope steeps down is where we could say that a peak exists
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            // there is an increasing trend / slope towards right
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            }
+            // there is an decreasing trend / slope towards right
+            // nums[mid] > nums[mid + 1]
+            else {
+                // because mid could itself be a peak according to the condition
+                high = mid;
+            }
         }
-        // there is an increasing trend / slope towards left
-        // nums[mid] > nums[mid + 1]
-        else{
-            high = mid;
-        }
-    }
-    return low;
+        // Eventually the search space shrinks to a single index (low == high).
+        // Since every elimination preserves the guarantee that at least one
+        // peak exists in the remaining search space, that remaining index must
+        // be a peak.
+        return low;
     }
 };
