0ca6cc88e4d5fec1fa3b5eb604e296367bf174f8
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-14 10:21:25 -0700

    Time: 0 ms (100.00%) | Memory: 12.5 MB (96.06%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+    // the elimination of either of the halves depends on the mid
+    // if nums[mid] < nums[mid + 1] we can see there is an increasing slope towards right
+    // because of this there are two possibilites
+    //1. if the slope keeps increasing to the very end, the last element becomes a peak
+    // because we know nums[-1] = nums[n] = inifinity (i.e first and last element are striclty greater than elements outside of the array)
+    // 2. the slope eventually steeps down somewhere, the point before the slope steeps down is where we could say that a peak exists
+    int n = nums.size();
+    int low = 0 , high = n - 1;
+    while(low < high){
+        int mid = low + (high - low) / 2;
+        // there is an increasing trend / slope towards right
+        if(nums[mid] < nums[mid + 1]){
+            low = mid + 1;
+        }
+        // there is an increasing trend / slope towards left
+        // nums[mid] > nums[mid + 1]
+        else{
+            high = mid;
+        }
+    }
+    return low;
+    }
+};
