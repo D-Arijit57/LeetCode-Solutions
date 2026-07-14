2e29174e2958c80cc03dafa1d7acbac1582e1f57
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-14 08:59:05 -0700

    Time: 0 ms (100.00%) | Memory: 13.7 MB (43.24%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        if(nums[n - 1] < target) return n;
+        // we're looking for the element that is greater than or equal to the target
+        // finding the closest to the target won't work here because if the difference is the same 
+        // we don't have any parameters to determine where it should be inserted
+        // we know that [0...low - 1] is where the elements < target
+        // [0...low] is unknown to us
+        // [0...high] is unknown and [high+1...] is where the elements are >= target
+        // so we need the index exactly high+1 that is the exact insert position
+        // so with each iteration we make sure we eliminate the unknown space to find out the high + 1 index
+        // why we do ? high = mid - 1 whenever we see nums[mid] >= target
+        // in this situation we just have the information that we are somewhere at the boundary where nums[mid] >= target, we don't know if its exactly high + 1 index or in simple words there might be another valid position further to the left
+        // so move towards left until low and high crosses each other 
+        // once they cross each other low is bound to point towards high + 1
+        int low = 0, high = n - 1;
+        while(low <= high){
+             int mid = low + (high - low) / 2;
+             // if the we have found the target we can directly return the index
+             if(nums[mid] >= target) high = mid - 1;
+             else low = mid + 1;
+        }
+        return low;
+    }
+};
