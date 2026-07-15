29b1975e12e2db9fcf3ef3b96e303f9533fdfe19
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-15 08:16:27 -0700

    Time: 0 ms (100%), Space: 63.6 MB (48.3%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
+        // the core idea is to make sure always eliminate the search space without a peak
+        // by doing that in each iteration we always move towards the search space where a peak exists 
+        // at the end the remaining element would be a peak 
         int n = arr.size();
         int low = 0, high = n - 1;
         while(low < high){
@@ -8,14 +11,10 @@ public:
             // these two following conditions makes sure
             // we are peaking sure we are always moving towards the space
             // where at least a peak exists
-            if(arr[mid] < arr[mid + 1]) {
-            
+            if(arr[mid] < arr[mid + 1]) 
                 low = mid + 1 ;
-            }
-            else{
-                
-                high = mid;
-            } 
+            // we do this because nums[mid] could be the peak itself
+            else high = mid;
         }
         return low;
     }
