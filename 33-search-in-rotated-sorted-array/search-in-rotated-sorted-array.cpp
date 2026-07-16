e58555d8a2a67e6752feea6023d8bf620539fee2
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-16 09:21:39 -0700

    Time: 0 ms (100.00%) | Memory: 15.4 MB (35.01%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,27 +1,37 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-       int n = nums.size();
-       int low = 0 , high = n - 1;
-       while(low <= high){
-        int mid = low + (high - low) / 2;
-        if(target == nums[mid]) return mid;
-        // if the element doesn't exist in the left half eliminate the left half
-        if(nums[low] <= nums[mid]){
-        if(target > nums[mid] || target < nums[low]){
-            low = mid + 1;
-        }
-        else high = mid - 1;
-        }
-        // if the element doesn't exist in the right half eliminate the right half
-        else {
-            if(target < nums[mid] || target > nums[high]){
-             high = mid - 1;
+        // while being inside [mid...high] determine if the target can exist in that interval
+        // same with [low...mid]
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            // [mid ... high] is sorted
+            // no rotation point inside
+            // determine if the target is inside [mid...high]
+            if(nums[mid] <= nums[high]){
+                if(nums[mid] == target) return mid;
+                // if the target is inside [mid...high]
+                else if(nums[mid] <= target && target <= nums[high]){
+                    low = mid + 1;
+                }
+                // otherwise
+                else high = mid - 1;
             }
-            else low = mid + 1;
+            // [mid...high] is not sorted
+            // there is a rotation point
+            // determine if the target belong inside [low...mid]
+            else {
+                if(nums[mid] == target) return mid;
+                // if the target is inside [low...mid]
+                else if(nums[low] <= target && target <= nums[mid]){
+                    high = mid - 1;
+                }
+                // otherwise
+                else low = mid + 1;
+            }
         }
-        
-       }
-       return -1;
-    }
+        return -1;
+        }
 };
