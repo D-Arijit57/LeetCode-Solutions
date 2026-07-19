fe42baffb68dbd1c36e3072d4ea14ea275f6eb9f
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-19 01:54:02 -0700

    Time: 26 ms (36.84%) | Memory: 107.5 MB (18.43%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.cpp
+++ 739-daily-temperatures/daily-temperatures.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        // in the stack each element should represent the indices
-        // since we need the distances between the last and next warmth day
-        // invariant : temperatures[prev] > temperatures[current]
-        // if we find a temperature that is warmer than the previous
-        // the previous one becomes useless
         int n = temperatures.size();
+        // since want distance
+        // we need the indices
         stack<int>st;
-        st.push(0);
-        vector<int>ans(n, 0);
-        int lastIndex = st.top();
-        for(int i = 1 ; i < n ; i++){
-            int current  = temperatures[i];
-            // st.top() < current -> next warmth day
-            // pop out the ones violating the invariant
-            while(!st.empty() && temperatures[st.top()] < current){
-                lastIndex = st.top();
+        vector<int>ans(n,0);
+        for(int i = 0 ; i < n; i++){
+
+            // pop - whenever you find a greater temperature than the top
+            // each incoming element can pop out multiple characters 
+            // so we need a while
+            while(!st.empty() && temperatures[st.top()] < temperatures[i]){
+                // we need to store the distance in the array before popping it out
+                // distance = currentWarmthDayIndex - lastWarmthdayIndex
+                // and since we are resolving it for the last warmth day
+                // use the st.top() -> index
+                ans[st.top()] = i - st.top();
                 st.pop();
-                // we are finding out the warmth day after lastIndex
-                // so store the next warmth day at lastIndex 
-                // distance = currentIndex - lastIndex
-                ans[lastIndex] = i - lastIndex;
             }
+            // push the new warmth day into the stack
             st.push(i);
         }
         return ans;
