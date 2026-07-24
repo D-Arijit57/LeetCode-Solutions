ac889cb30585d907707bf6628109da3a58c4150d
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-24 09:10:04 -0700

    Time: 10 ms (70.65%) | Memory: 35 MB (54.27%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canShip(vector<int>& weights, int days, int cap){
+        int n = weights.size();
+        // we atleast need 1 day to ship one package so it starts from 1 
+        int currWeight = 0, dayUsed = 1;
+        for(int i = 0 ; i < n ; i++){
+            // if weights[i] makes current weight reaches the limit of the capactiy
+            // start a new day
+            if(currWeight + weights[i] > cap){
+                currWeight = 0;
+                dayUsed++;
+            }
+            currWeight += weights[i];
+        }
+        return dayUsed <= days;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        // the search space is the capacity
+        // atleast we can ship a package of weight 1 
+        // at max the capacity can be sum of all the weights
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+        int ans = 0;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            // if you have found a capacity that works then look for a more valid one in the left
+            if(canShip(weights, days,  mid)){
+                ans = mid;
+                high = mid - 1;
+            }
+            // the capacity is too low, we need a larger one
+            else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
