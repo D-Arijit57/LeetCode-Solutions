f567eae7dcd23ed8c2f3493fee3d82abdd126761
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-25 12:45:44 -0700

    Time: 0 ms (100.00%) | Memory: 23.1 MB (10.77%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.cpp
+++ 1028-interval-list-intersections/interval-list-intersections.cpp
@@ -1,38 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-        // two pointer approach 
-        // brute force : take one interval and compare it with the rest of the list's interval O(N * M) N,M : size of the first and second list respectively
-        // better approach : take i,j as two pointers from interval list 1 and 2 
-        // compare them if the intersect then store the interval in the answer list 
-        // if they don't then make sure to move the pointer which cannot intersect the other interval because since the array is sorted it won't intersect in future as well.
-        // Time complexity : O(N+M)
-            if(firstList.empty() || secondList.empty()) return {};
-            int n = firstList.size();
-            int m = secondList.size();
-            vector<vector<int>>ans;
-            int first = 0, second = 0;
-            while(first < n && second < m){
-                // initialize the lists
-                vector<int>&a = firstList[first];
-                vector<int>&b = secondList[second];
-
-                int start = max(a[0],b[0]);
-                int end = min(a[1], b[1]);
-
-                if(start <= end){
-                    ans.push_back({start, end});
-                }
-
-                // pointer movement
-                // if a finishes first (cannot intersect)
-                if(a[1] < b[1]){
-                    first++;
-                }
-                // otherwise
-                else second++;
+        // intersection is defined by :
+        // {max(start), min(end)}
+        // we can only find the intersection if two intervals overlap otherwise we can't
+        // two intervals overlap when first.end >= second.start
+        int n = firstList.size();
+        int m = secondList.size();
+        if(m == 0  || n == 0) return {};
+        vector<vector<int>>ans;
+        int left = 0, right = 0;
+        while( left < n && right < m){
+            int start1 = firstList[left][0];
+            int start2 = secondList[right][0];
+            int end1 = firstList[left][1];
+            int end2 = secondList[right][1];
+            // if they overlap
+            // one interval might have intersections with multiple intervals
+            // overlap condition only when start1 <= end2 && start2 <= end1
+            if(start1 <= end2 && start2 <= end1){
+                // find the intersection and push it in the ans
+                int intersection_start = max(firstList[left][0], secondList[right][0]);
+                int intersection_end = min(firstList[left][1], secondList[right][1]);
+                ans.push_back({intersection_start,intersection_end});
             }
-
-        return ans;
+            // move the pointer which interval end's first
+            if(end1 <= end2) left++;
+            else right++;
         }
+        return ans;
+    }
 };
