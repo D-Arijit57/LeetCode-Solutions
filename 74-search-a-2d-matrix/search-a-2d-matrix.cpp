660f35c5aac5da8eccd3584a17657ca82c0ea2b2
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-29 09:14:02 -0700

    Time: 0 ms (100.00%) | Memory: 13.4 MB (45.55%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -14,8 +14,13 @@ public:
             int mid = low + (high - low) / 2;
             // to calculate that we have flatten the indices
             // we can do that by dividing the index by the no of cols
-            // it simply tanslates how many complete rows have I passed 
+            // it simply tanslates how many complete rows have we passed 
             int curr_row = mid / cols;
+            // we have to make sure the col is flattened as well or it may generate out of bound errors
+            // for example, we have mid = 5 and we are at row = 0
+            // indices are 0 1 2 3 , mid = 5 accessing it produces out of bound
+            // so we have to divide it by column size to flatten it and map it to the exact distance its far the start of the row
+            int curr_col = mid % cols;
             if(matrix[curr_row][mid % cols] > target) high = mid - 1;
             else if(matrix[curr_row][mid % cols] < target) low = mid + 1;
             else return true;
