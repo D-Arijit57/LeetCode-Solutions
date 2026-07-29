49cced782fe4739bf72b07fde71005a518aa40a3
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-29 09:50:37 -0700

    Time: 0 ms (100%), Space: 13.3 MB (45.55%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,33 +1,39 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-       
-        // the search space is the entire matrix
-        // it starts from first element of the first row till last element of the last row 
-        // since the matrix is sorted in non-decreasing order
-        // it already has monotonicity
+
         int rows = matrix.size();
         int cols = matrix[0].size();
-        int low = 0, high = rows * cols  - 1;
-        while(low <= high){
-            // mid represents the index of the candidate element
-            // but it doesn't in which row it is it
+
+        // Instead of binary searching each row, treat the entire matrix as a
+        // virtually flattened sorted array of size (rows * cols).
+        int low = 0;
+        int high = rows * cols - 1;
+
+        while (low <= high) {
+
+            // 'mid' represents the candidate's index in the virtual 1D array.
             int mid = low + (high - low) / 2;
-            // Treat the matrix as a virtually flattened sorted array.
-            // Convert the flattened index back to (row, col).
-            // we can do that by dividing the index by the no of cols
-            // it simply tanslates how many complete rows have we passed 
+
+            // Convert the global (flattened) index back into matrix coordinates.
+            //
+            // Division tells us how many complete rows have been crossed,
+            // which gives the row containing the element.
             int curr_row = mid / cols;
-            // Convert the flattened index back to the column.
-            // It tells us how far into the current row the element lies.
-            // for example, we have mid = 5 and we are at row = 0
-            // indices are 0 1 2 3 , mid = 5 accessing it produces out of bound
-            // take modulo with the number of columns
+
+            // Modulo converts the global index into a local column index.
+            // It measures the offset from the beginning of the current row,
+            // effectively resetting each row's indexing to start from 0.
             int curr_col = mid % cols;
-            if(matrix[curr_row][mid % cols] > target) high = mid - 1;
-            else if(matrix[curr_row][mid % cols] < target) low = mid + 1;
-            else return true;
+
+            if (matrix[curr_row][curr_col] > target)
+                high = mid - 1;
+            else if (matrix[curr_row][curr_col] < target)
+                low = mid + 1;
+            else
+                return true;
         }
+
         return false;
     }
 };
