31bf4428e47c0680bdfabe8b5c9ae9afb8d0f4bf
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-29 09:15:00 -0700

    Time: 0 ms (100%), Space: 13.3 MB (79.66%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+       
         // the search space is the entire matrix
         // it starts from first element of the first row till last element of the last row 
         // since the matrix is sorted in non-decreasing order
@@ -12,14 +13,16 @@ public:
             // mid represents the index of the candidate element
             // but it doesn't in which row it is it
             int mid = low + (high - low) / 2;
-            // to calculate that we have flatten the indices
+            // Treat the matrix as a virtually flattened sorted array.
+            // Convert the flattened index back to (row, col).
             // we can do that by dividing the index by the no of cols
             // it simply tanslates how many complete rows have we passed 
             int curr_row = mid / cols;
-            // we have to make sure the col is flattened as well or it may generate out of bound errors
+            // Convert the flattened index back to the column.
+            // It tells us how far into the current row the element lies.
             // for example, we have mid = 5 and we are at row = 0
             // indices are 0 1 2 3 , mid = 5 accessing it produces out of bound
-            // so we have to divide it by column size to flatten it and map it to the exact distance its far the start of the row
+            // take modulo with the number of columns
             int curr_col = mid % cols;
             if(matrix[curr_row][mid % cols] > target) high = mid - 1;
             else if(matrix[curr_row][mid % cols] < target) low = mid + 1;
