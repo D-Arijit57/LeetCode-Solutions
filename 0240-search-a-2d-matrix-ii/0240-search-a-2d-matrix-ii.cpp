87cc875cb9f24c47f21d8914199f2e142112de10
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-30 09:29:20 -0700

    Time: 104 ms (10.84%), Space: 18.8 MB (39.47%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool doExists(vector<int>&row, int target){
+        int n = row.size();
+        int low = 0, high = n - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(row[mid] > target) high = mid - 1;
+            else if(row[mid] < target) low = mid + 1;
+            else return true;
+        }
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int row = 0;
+        while(row < rows){
+            if(doExists(matrix[row], target)){
+                return true;
+            }
+            else row++;
+        }
+    return false;
+    }
+};
