d45d68482fed4512ad28ad375b11a4974b61bdb6
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-07-30 10:27:37 -0700

    Time: 51 ms (70.18%), Space: 18.7 MB (68.72%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // staire
+        // staircase 
         int rows = matrix.size();
         int cols = matrix[0].size();
+
+
+        // Start from the top-right corner.
+        // This corner is special because:
+        // - Every element to the left is smaller.
+        // - Every element below is larger.
         int row = 0, col = cols - 1;
         while(row < rows && col >= 0){
             if(matrix[row][col] == target){
                 return true;
             }
+
+            // Current element is too large.
+            // Since every element below it in the same column is even larger,
+            // the target cannot exist in this column.
+            // move left 
             else if(matrix[row][col] > target){
-                col--;
+                col--; 
             }
-            else row++;
+
+            // Current element is too small.
+            // Since every element to its left in the same row is even smaller,
+            // the target cannot exist in this row.
+            // move down 
+            else row++; 
         }
         return false;
     }
