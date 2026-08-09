fafcabedade04c57e0ecd98fee0faa29423439ed
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-09 09:00:59 -0700

    Time: 2 ms (23.76%) | Memory: 27.1 MB (76.48%) - LeetSync

--- 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/maximum-subarray-sum-with-one-deletion.cpp
@@ -5,13 +5,13 @@ public:
         int n = arr.size();
 
         // the deletion is optional, that too is once
-        // we should keep the sum with the highest value
-        int keep = arr[0], ans = arr[0], one_delete = 0;
+        // we should no_delete the sum with the highest value
+        int no_delete = arr[0], ans = arr[0], one_delete = 0;
         // how can exactly one deletion happen at index i ?
         // conceptually if the current element gets one_delete we are left with the sum of the previous subarray without the current one
         // that is the previous_no_delete
         for(int i = 1 ; i < n ; i++){
-            int previous_no_delete = keep;
+            int previous_no_delete = no_delete;
             int previous_one_delete = one_delete;
             // we have to deletion conditions either one delete or no delete
             // so if there's no deletion we can extend the preivous subarray sum
@@ -19,10 +19,10 @@ public:
             one_delete = max(previous_no_delete, previous_one_delete + arr[i]);
             
             // base kadane's checking if it should start a new sum or extend the previous one
-            keep = max(arr[i],  previous_no_delete + arr[i]);        
+            no_delete = max(arr[i],  previous_no_delete + arr[i]);        
             
             // check if the maximum sum is with one deletion or no deletion till this index i 
-            ans = max({ans, keep ,one_delete});
+            ans = max({ans, no_delete ,one_delete});
         }
         return ans;
     } 
