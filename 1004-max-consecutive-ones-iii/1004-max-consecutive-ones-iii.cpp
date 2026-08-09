89e8829c01c0f2f0d63eb523a181e0fffaa5675b
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-09 08:11:33 -0700

    Time: 0 ms (100%), Space: 69.6 MB (34.59%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        // using maps this time
-        
-        // int n = nums.size();
-        // unordered_map<int, int>freq;
-        // int left = 0, maxLen = 0;
-        // for(int right = 0 ; right < n ; right++){
-        //     freq[nums[right]]++;
-        //     if(nums[right] == 0){
-        //         while(freq[nums[right]] > k){
-        //             freq[nums[left]]--;
-        //             left++;
-        //         }
-        //     }
-        //     maxLen = max(maxLen , right - left + 1);          
-        // }
-        // return maxLen;
-
-        // since we know we only need to count 
-        // and check if the number of zeros > k (to skip invalid window)
-        // we can just keep a counter for the zero
+        // we need to check only for zeros
+        // the count of zeros inside the window should be always <= k
+        // if it breaks we should shrink the window
         int n = nums.size();
-        int left = 0, zeroCnt = 0, maxlen = 0;
+        int left = 0, zeros = 0;
+        int ans = INT_MIN;
         for(int right = 0 ; right < n ; right++){
-            if(nums[right] == 0) zeroCnt++;
-            while(zeroCnt > k){
-                if(nums[left] == 0) zeroCnt--;
-                left++;
+            // expand the window
+            if(nums[right] == 0) zeros++;
+            // shrink the window if there are more zeros than ks
+            while(zeros > k){
+                if(nums[left] == 0) zeros--;
+                left++;                
             }
-            maxlen = max(maxlen , right - left + 1);
-        }
-        return maxlen;
+            // for every valid window, update the maximum length
+            ans = max(ans, right - left + 1);
+        }      
+        return ans;
     }
 };
