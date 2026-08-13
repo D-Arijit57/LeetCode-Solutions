a099fd8a2380f0bdefb85ccb9a8b38aea1a1e7e8
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-13 09:53:39 -0700

    Time: 0 ms (100%), Space: 10.9 MB (44.16%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -11,24 +11,29 @@
  */
 class Solution {
 public:
-    vector<int>ans;
-    void preOrderDfs(TreeNode* root){
-        // if there's no next node return null
-        if(root == nullptr) return ;
-
-        TreeNode* node = root;
-        // first process the current node
-        ans.push_back(node->val);
-
-        // go left
-        preOrderDfs(node->left);
-
-        // go right
-        preOrderDfs(node->right);
-
-    }
     vector<int> preorderTraversal(TreeNode* root) {
-        preOrderDfs(root);
+        if(root == nullptr) return {};
+        // iterative approach
+        vector<int>ans;
+        // the stack should only contain the valid nodes
+        // the stack represents the set of un-processed nodes
+        // more precisely the nodes that we have discovered but we haven't processed yet
+        // pre-order traversal :  node -> left -> right
+        stack<TreeNode*>st;
+        st.push(root);
+        // iterate till all of the nodes are processed
+        while(!st.empty()){
+            TreeNode* curr = st.top();
+            // pop when you it the nullptr, previous node was a leaf node
+            st.pop();
+            ans.push_back(curr->val);
+            // since stack is LIFO, we need to -
+            // first do push(right) then push(left)
+            // because we want the first child to be left and the second one to be right
+            // because of the LIFO nature of stack it might reverse it so we push it in reverse manner
+            if(curr->right) st.push(curr->right);
+            if(curr->left) st.push(curr->left);
+        }   
         return ans;
     }
 };
