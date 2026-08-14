63c90d5394e7a5db1c9ab55cc724fcba0cb799f9
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-14 08:21:33 -0700

    Time: 0 ms (100%), Space: 9.2 MB (77.33%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -4,6 +4,7 @@ public:
         // invariant : each character inside the window must have a frequency <= 2
         // the freq[incoming_char] > 2 then we have to shrink the window from the left
        int n = s.size();
+       // strings contains only the lowercase characters: size = 26
        vector<int>freq(26,0);
        int left = 0, ans = INT_MIN;
        for(int right = 0 ; right < n ; right++){
