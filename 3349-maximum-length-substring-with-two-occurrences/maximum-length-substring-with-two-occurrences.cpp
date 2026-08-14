5ccdcf56b025280f1fba37b5f7ef24d2cf1f0a63
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-14 08:21:15 -0700

    Time: 3 ms (42.93%) | Memory: 9 MB (87.78%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -4,15 +4,15 @@ public:
         // invariant : each character inside the window must have a frequency <= 2
         // the freq[incoming_char] > 2 then we have to shrink the window from the left
        int n = s.size();
-       unordered_map<int,int>freq;
+       vector<int>freq(26,0);
        int left = 0, ans = INT_MIN;
        for(int right = 0 ; right < n ; right++){
             // expand the window 
-            freq[s[right]]++;
+            freq[s[right] - 'a']++;
 
             // shrink the window 
-            while(freq[s[right]] > 2){
-                freq[s[left]]--;
+            while(freq[s[right] - 'a'] > 2){
+                freq[s[left] - 'a']--;
                 left++;
             }
 
