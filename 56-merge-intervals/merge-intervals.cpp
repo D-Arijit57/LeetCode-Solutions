7ddff1d4c734ec912f9b76cb166615cdfa139dd3
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-15 22:35:19 -0700

    Time: 4 ms (66.30%) | Memory: 23.9 MB (31.48%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // sort so that we can identify if there are any overlaps
-        // to find if there is any interval : 
-        // if (currentFirst <= mergedLast) then merge
-        sort(intervals.begin(),intervals.end());
+        // we should sort the intervals in increasing order
+        // or else the ones that start before an interval would be missed
+        // e.g [[4,7],[1,4]] 
+        sort(intervals.begin(), intervals.end());
+        int n = intervals.size();
         vector<vector<int>>ans;
         ans.push_back(intervals[0]);
-        for(int i = 1 ; i < intervals.size(); i++){
-            if(intervals[i][0] <= ans.back()[1]){
+        for(int i = 1; i < n ; i++){
+            // if the incoming interval overlaps
+            // merge it
+            // the end becomes max end between two of the intervals
+            // because then only it can cover the overlapped inteval
+            if( ans.back()[1] >= intervals[i][0]){
                 ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-            else{
-                ans.push_back(intervals[i]);
-            }
+            // if they don't overlap simply push the interval into the ans
+            else ans.push_back(intervals[i]);
         }
         return ans;
     }
