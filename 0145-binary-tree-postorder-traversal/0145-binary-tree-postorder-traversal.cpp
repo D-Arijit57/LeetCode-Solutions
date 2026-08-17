2e92018b07600911b4c313e870dc833afa8a6a98
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-17 09:17:23 -0700

    Time: 0 ms (100%), Space: 11 MB (74.49%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,31 +1,67 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
+ * Definition for a binary tree curr.
+ * struct Treecurr {
  *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     Treecurr *left;
+ *     Treecurr *right;
+ *     Treecurr() : val(0), left(nullptr), right(nullptr) {}
+ *     Treecurr(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     Treecurr(int x, Treecurr *left, Treecurr *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void postOrderDfs(TreeNode *root, vector<int>&ans){
-        if(root == nullptr) return;
-        TreeNode* node = root;
-        // move left
-        postOrderDfs(node->left, ans);
-
-        //move right
-        postOrderDfs(node->right,ans);
-
-        //store the node
-        ans.push_back(node->val);
-    }
     vector<int> postorderTraversal(TreeNode* root) {
+        // edge case : empty tree
+        if(root == nullptr) return {};
+        // postorder sequence : left -> right -> order
+        // unlike the inorder or preorder sequence traversal 
+        // the stack alone doesn't give us the information if a root's left and right subtree
+        // has been completely processed 
+        // because of that
+        // we would need a stack + extra pointer
+        // where stack would represent the ancestors waiting to be processed
+        // the extra pointer: prev would represent the subtree we just processed
+        // the our framework says that
+        // explore a curr's left subtree, once done come back check 
+        // prev == curr -> left ? explore right : explore left 
+        // then after exploring right if it hasn't been explored
+        // check prev == curr ->right ? process curr : explore right
+        stack<TreeNode*>st;
+        TreeNode* curr = root;
+        TreeNode* prev = nullptr;
         vector<int>ans;
-        postOrderDfs(root,ans);
+        // till all the currs are processed
+        while(!st.empty() || curr){
+            // reach the end of the left subtree
+            while(curr){
+                st.push(curr);
+                curr = curr->left;
+            }
+            // once you reach the end of the left subtree
+            // go back , [the top of the stack if the ancestor we are returning to]
+            curr = st.top();
+            // check if the right exists of the ancestor
+            // also check if we have already explored its right subtree
+            if(curr->right && prev != curr->right){
+                curr = curr->right;
+            }
+            // in-case we have alread explored its left and right subtree
+            // now the curr itself can be processed
+           else{
+                // remember the subtree we just completed 
+                prev = curr;
+
+                // add the value to the sequence
+                ans.push_back(curr->val);
+                // since processed, remove it from the stack
+                st.pop();
+                
+                // since currently there is no subtree right now
+                // curr is set to nullptr 
+                curr = nullptr;
+           }
+        }
         return ans;
     }
 };
