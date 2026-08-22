d66f7a81d1c1624ed92844553c5a6b3e28b5d8a8
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-22 08:54:23 -0700

    Time: 7 ms (86.78%) | Memory: 14 MB (50.50%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,34 +1,22 @@
-// Revision June 20th : 
-// Mistakes :
-// 1. Storing wrong difference instead of the sum
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        // before using two pointers we need to make sure the array is sorted
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        int ans = 0, minDiff = INT_MAX;
+        int minDiff = INT_MAX, ans = 0;
         for(int i = 0 ; i < n ; i++){
-            // skip the same triplets
             if(i > 0 && nums[i] == nums[i-1]) continue;
             int left = i + 1, right = n - 1;
-            // find the sum 
-            // if sum == target then return
-            // otherwise update the difference between the target and the sum
-            // the minimum the distance the more closer the sum is 
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
                 int diff = abs(target - sum);
-
-                // once you find a new minimum distance update the answer as well as the minDiff seen till now 
                 if(diff < minDiff){
-                    minDiff = diff;
                     ans = sum;
+                    minDiff = diff;
                 }
-                
-                if(sum == target) return sum;
+                if(sum > target) right--;
                 else if(sum < target) left++;
-                else right--;
+                else return sum;
             }
         }
         return ans;
