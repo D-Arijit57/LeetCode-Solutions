f7da890e951ddcfef1db54d0514d3628a2554a53
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-22 08:57:04 -0700

    Time: 7 ms (86.78%), Space: 14 MB (50.5%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        // closest sum means the difference between the sum and the target is minimum
+        // so we are actually looking for the sum where the distance will be minimum
+        // if we find the target we immadiately return it
+        // otherwise doesn't matter if its greater or smaller we always update the distance
+        // if the current distance is the new smallest one compared to the one we have seen so far
+        // the basic principle stays the same because monotonicity of sorted space
+        // that is if we find a sum that is smaller than the target we go right
+        // if its greater we go left
+        // as the more we go right there are increasing elements
+        // the more we go left there are decreasing elements
         sort(nums.begin(), nums.end());
         int n = nums.size();
         int minDiff = INT_MAX, ans = 0;
