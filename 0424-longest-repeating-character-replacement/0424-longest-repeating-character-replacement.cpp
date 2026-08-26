6611f1c01d95e8c36e5caa2483405e44e7cf9e93
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-26 13:56:48 -0700

    Time: 6 ms (47.13%), Space: 11.1 MB (19.39%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        //  WHY STALE MAXFREQ DOESN'T MAKE THE ANSWER GO WRONG  
-        //Since we've already observed a frequency of maxFreq, 
-        // we know such a strong character concentration existed at some point. We can safely use that historical best as a certificate.
-        // It may delay shrinking and temporarily keep invalid windows,
-        // but it cannot cause us to miss the optimal answer or report a length that was never achievable.
-
-
-        // It's a valid window if requirement for replacement is less or equal to k 
-        // requirement = windowsize - maxFreq (inside the frequency map)
+        // invariant : requirement <= k
+        // if requiremnet > k shrink the window because that's a invalid one
         int n = s.size();
-        // since the string only contains uppercase characters
-        vector<int>freq(26,0);
-        int left = 0, maxFreq = INT_MIN, maxlen = -1;
-        for(int right = 0 ; right < n ; right++){
-            freq[s[right] - 'A']++;
-            // keep updating the max freq after every updation
-            maxFreq = max(maxFreq,freq[s[right] - 'A']);
-            // shrinking logic
-            while((right- left + 1) - maxFreq > k){
-                   freq[s[left] - 'A']--;
-                   left++;             
+        int left = 0;
+        unordered_map<char, int>freq;
+        int ans = INT_MIN, maxFreq = 0;
+        for(int right = 0 ; right < n  ; right++){
+            // expand the window
+            freq[s[right]]++;
+            // shrink the invalid window 
+            // requirement = windowSize - frequency of the most frequent character
+            maxFreq = max(maxFreq, freq[s[right]]);
+            while((right - left + 1) - maxFreq > k){
+                freq[s[left]]--;
+                left++;
             }
-            maxlen = max(maxlen, right - left + 1);          
+            // update longest length
+            ans = max(ans, right - left + 1);
         }
-        return maxlen == -1 ? -1 : maxlen;
+        return ans;
     }
 };
