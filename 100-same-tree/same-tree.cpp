3b5c824baa40f98d6076a06e2d76c94c9c622d38
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-27 08:58:12 -0700

    Time: 0 ms (100.00%) | Memory: 13 MB (13.18%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -12,8 +12,8 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        // the queue represents the pair of nodes
-        // potentially same from the two trees 
+        // the queue represents the pair of nodes from the two trees 
+        // which are potentially same
         queue<pair<TreeNode*, TreeNode*>>same_nodes;
         same_nodes.push({p,q});
         while(!same_nodes.empty()){
