6e23cb1c3b79a06f6bff0e22565efbba646983a2
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-27 13:02:32 -0700

    Time: 5 ms (29.22%) | Memory: 11.5 MB (47.51%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,60 +1,77 @@
-// parse , process, build the string
 class Solution {
 public:
     string simplifyPath(string path) {
-        // The stack stores the directories that belong to the simplified path after processing the current path prefix.
-        // atomic unit of this problem - directory 
-        // each incoming element can be '/','.' or a character 
-        // each element can add up to string or cancel 1 or more than 1 elements depending on the top of the stack which element it is
-        // stack will represent the simplified path 
-        // after the operation we can derive the simplified path 
-        // simulation problem
-        int n = path.size();
-        string simplified_path = "";
-        stack<string>st; // stores directory
-        // home , foo, documents instead h o m e , f o o
-        // so use a string
-        string token = ""; 
-        for(int i = 0 ; i <= n ; i++){
-          // either we have reached the end or we have found '/'
-          if(i == path.size() || path[i] == '/'){
-            // process the completed token
-                if(token.empty() || token == "."){
-                    // ignore, do not do anything
-                    // . represents the current directory 
-                }
-            else if(token == ".."){
-                if(!st.empty()){
-                    st.pop();
-                }
-            }
-            // we have a complete directory name
-            // push it in the stack
-            else{
-                st.push(token);
-            }
+        // build the token -> directory name, push them in the stack
+        // build the clean path
+      int n = path.size();
+      // the stack represents the current directory path
+      stack<string>st;
+      string ans = "",  token = "";
+      for(int i = 0 ; i < n ; i++){
+        // build the token
+        if(path[i] != '/') token += path[i];
+        // . means the current repository
+        // but we have to check if the next one is . or not 
+        // as that changes the decision, because that means the prev directory
+        // so we cannot directly add the next one
+        else {
+            
+        if(token == "" || token == ".") {
+            // don't forget to clear the token since we don't want to carry forward this .
             token.clear();
-          }
-          else{
-            // build the current directory name
-            token += path[i];
-          }
-          
+            continue;
+        } 
+
+        // .. -> represents the previous directory : bin/usr/doc/.. -> go back to usr
+        // remove the previous directory from the stack
+        // if the stack isn't empty 
+        // to go back to the parent directory 
+        if(token == ".."){
+            if(!st.empty()) st.pop();
+            // also clear the token since .. doesn't represent a dir name
+            // and we don't want to carry forward this .. with the actual dir name
+            token.clear();
+        } 
+
+        // '/' marks the end and the start of a new directory
+        // edge case for a path /a/b/c : c never gets processed since there is no ending '/'
+        // directing that c should get processed 
+        // so we should push c explicitly after the loop ends
+        else {
+             st.push(token);
+             token.clear();
+            } 
         }
-        // rebuild the string from the stack
-        vector<string>directories;
+      }
+      // after a loop finishes if token isn't empty means that we have a remaining token
+      // that couldn't get process because there is no ending '/' after last dir name
+      // which the indicator of a complete dir name
+      // so push it explicitly after the loop
+      if(token != "") {
+        if(token == ".") token.clear();
+        else if(token == ".."){
+            if(!st.empty()) st.pop();
+            token.clear();
+        }
+        else {
+            st.push(token);
+            token.clear();
+        }
+      }
+
+      // now we have all the directory names
+      // build the clean part according to the rules
+        vector<string>dirs;
         while(!st.empty()){
-            directories.push_back(st.top());
+            string s = st.top();
+            dirs.push_back(s);
             st.pop();
         }
-        reverse(directories.begin(), directories.end());
-
-        // build the simplified path, directories separated by a path
-        for(const string &str : directories){
-            simplified_path += "/";
-            simplified_path += str;
+      // build the path with the finalized dirs 
+        for(int i = dirs.size() - 1; i >= 0 ; i--){
+            ans += "/" + dirs[i];
         }
 
-        return simplified_path.empty() ? "/" : simplified_path;
+     return ans.empty() ? "/" : ans;
     }
 };
