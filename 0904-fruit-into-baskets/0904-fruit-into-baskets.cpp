b346148af8dd309dc7d3da3c237456cb596243ce
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-30 08:15:11 -0700

    Time: 28 ms (89.86%), Space: 84.9 MB (63.12%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        // valid window would where window.size() <= 2
+        // since we have two baskets and each basket can only hold a single type of fruit
         int n = fruits.size();
-        unordered_map<int, int>freq;
-        int left = 0 ;
-        int maxFruits = INT_MIN;
-        for(int right = 0 ; right < n ; right++){
+        unordered_map<int,int>freq;
+        // the left boundary of the window
+        int left = 0;
+        int ans = INT_MIN;
+        for(int right = 0; right < n ; right++){
             freq[fruits[right]]++;
 
-            // shrink the if the window is invalid
+            // shrink: invalid window : window.size() > 2
             while(freq.size() > 2){
                 freq[fruits[left]]--;
-                // if the count becomes zero
-                // remove it from the map
-                if(freq[fruits[left]] == 0 ) freq.erase(fruits[left]);
+                if(freq[fruits[left]] == 0) freq.erase(fruits[left]);
                 left++;
+                // if the frequency becomes zero : that is the type of fruit doesn't exist in the window
+                // then make sure you remove it from the window
+               
             }
-            maxFruits = max(maxFruits, right - left + 1);
+            // for each valid window, we're going to update the maximum length
+            ans = max(ans, right - left + 1);
         }
-    return maxFruits;
+
+        return ans;
     }
 };
