cf0bb97f95a177a79b29720bb7e2f863e9ad44f1
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-30 08:25:59 -0700

    Time: 59 ms (29.82%), Space: 19.3 MB (27.97%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        // edge case : if the string is empty
+        if(s.empty()) return 0;
+        // abcabcbb
+        // {a : 1, b : 1, c : 1}
+        // {a :2 , b: 1, c: 1} // -> a is getting duplicated 
+        // shrink the window
+        // valid window : where every character as at most the fequency as one its either 0 or 1 
         int n = s.size();
         unordered_map<char, int>freq;
-        int left = 0, maxLen = 0;
-        for(int right = 0 ; right < n ; right++){
+        int left = 0;
+        int ans = INT_MIN;
+        for(int right = 0; right < n ; right++){
             freq[s[right]]++;
 
-            // check if the current character already exists in the window
-            if(freq[s[right]] > 1){
-
-                // if it does we need to shrink the window till the duplicate gets removed
-                while(freq[s[right]] > 1){
-                    freq[s[left]]--;
-                    // in case it becomes zero, remove it from the map
-                    if(freq[s[left]] == 0) freq.erase(s[left]);
-                    left++;
-                }
+            // shrink invalid window : where the incoming character, increases a character's frequency to more than 1
+            // valid freq[right] <= 1
+            while(freq[s[right]] > 1){
+                // shrink from the left to maintain the contigious property
+                freq[s[left]]--;
+                left++;
             }
-            // shrinking logic
-            maxLen = max(maxLen, right - left + 1);
+            // for every valid window update the length of the substring
+            ans = max(ans , right - left + 1);
         }
-        return maxLen;
+        return ans;
     }
 };
