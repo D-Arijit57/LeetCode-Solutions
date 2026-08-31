1a6f42e907546f35de1ce55ba5e3120d52cc76f8
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-31 08:58:23 -0700

    Time: 3 ms (27.57%), Space: 97.5 MB (25.89%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        // the best profit would be lowest buying price and highest selling price
+        // we maintain a min_seen variable to store the minimum price we have seen so far
+        // so check the profit by finding the difference between the prices[i] - min_seen
         int n = prices.size();
         int min_seen = INT_MAX;
         int profit = 0;
