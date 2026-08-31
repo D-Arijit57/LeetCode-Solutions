f269d68ed87559e4f63b2b8f880d064ec92de372
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-31 08:38:30 -0700

    Time: 1 ms (87.50%) | Memory: 11.6 MB (63.80%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -1,42 +1,55 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int n = s.size();
-        int m = p.size();
-        if(m > n) return {};
-        int matches = 0;
-        vector<int>windowFreq(26,0);
-        vector<int>targetFreq(26,0);
-        vector<int>ans;
-        // updating the target freq
+       // edge case : if p is greater in length compared to s
+       // its impossible for s to have all of its anagrams
+       if(p.size() > s.size()) return {};
+
+       int n = s.size();
+       int m = p.size();
+
+       vector<int>ans;
+       
+       // we are maintaining two frequency maps to compare 
+       // the current vs desired frequency
+       vector<int>windowFreq(26,0);
+       vector<int>targetFreq(26,0);
+        
+        // load targetFreq and windowFreq with p's and s's frequency accordingly
+        // so that we can check for the first window 
         for(int i = 0 ; i < m ; i++){
             targetFreq[p[i] - 'a']++;
             windowFreq[s[i] - 'a']++;
         }
-
-        // checking for the first window 
+        // check for the first window, for all the 26 characters 
+        // if the window has the desired frequency the matches will stay 26 always
+        // if there's any discrepency (< 26) means that the window's frequency is not the desired frequency
+        int matches = 0;
         for(int i = 0 ; i < 26 ; i++){
-            if(windowFreq[i] == targetFreq[i]) matches++;
+            if(targetFreq[i] == windowFreq[i]) matches++;   
         }
-        
         if(matches == 26) ans.push_back(0);
 
-        // for the rest of the window
+        // now since we have checked for the first window
+        // check for the entire string after the length that we just checked
         for(int i = m ; i < n ; i++){
-            int left = s[i] - 'a';
-            int right = s[i - m] - 'a';
-            // transition mechanism
-            // check if the count before and after the window before updating 
-            if(windowFreq[left] == targetFreq[left]) matches--;
-            windowFreq[left]++;
-            if(windowFreq[left] == targetFreq[left]) matches++;
+            int left = s[i - m] - 'a';
+            int right = s[i] - 'a';
 
-            // check if the count before and after window before removing a character
+            // check for the character entering the window
             if(windowFreq[right] == targetFreq[right]) matches--;
-            windowFreq[right]--;
+            windowFreq[right]++;
             if(windowFreq[right] == targetFreq[right]) matches++;
 
-            if(matches == 26 ) ans.push_back(i - m + 1);
+
+            // check for the character leaving the window
+            if(windowFreq[left] == targetFreq[left]) matches--;
+            windowFreq[left]--;
+            if(windowFreq[left] == targetFreq[left]) matches++;
+            
+            // if only matches stays the same, then only we know 
+            // the window is valid and the substring is a anagram of p
+            if(matches == 26) ans.push_back(i - m + 1);
         }
         return ans;
     }
