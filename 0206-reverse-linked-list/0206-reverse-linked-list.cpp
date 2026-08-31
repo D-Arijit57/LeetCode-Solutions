6012b4c49eb4cbfcfac1a722b47ded14ef852427
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-08-30 23:41:55 -0700

    Time: 0 ms (100%), Space: 13.4 MB (41.52%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -14,9 +14,13 @@ public:
         ListNode* prev = nullptr;
         ListNode* curr = head;
         while(curr){
+            // save the next check point, so that we can move to the next after reversal
             ListNode* next = curr->next;
+            // reconnect the current node 
             curr->next = prev;
+            // the current one (already reversed) becomes the previous 
             prev = curr;
+            // since we saved the checkpoint, move to it 
             curr = next;
         }
         return prev;
