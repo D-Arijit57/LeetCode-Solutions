5d208d8f9c4a56d9a6db8977629263291ec6ea9c
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-09-08 09:09:42 -0700

    Time: 57 ms (53.57%), Space: 24 MB (55.6%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        // intersection : not the same value of two nodes
+        // it means the two nodes point to the same location in memory
+        // Each pointer traverses both lists once. The redirection cancels out the difference in their lengths, so if an intersection exists, both pointers reach the shared node at the same time.
+        ListNode* list1 = headA;
+        ListNode* list2 = headB;
+
+        // fast and slow pointer approach
+        while(list1 || list2){
+            if(list1 == list2) return list1;
+            // compare one list's node with every node of the other list
+            // if one reaches the the end redirect it to the head of the list
+            list1 = list1 ? list1->next : headB;
+            list2 = list2 ? list2->next : headA;   
+        }
+        return nullptr;
+    }
+};
