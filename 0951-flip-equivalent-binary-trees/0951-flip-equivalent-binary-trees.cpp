bd014a51e47e9a9adb120e658f6373147409a2b0
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-09-10 15:44:21 -0700

    Time: 0 ms (100%), Space: 15.5 MB (9.72%) - LeetHub

---
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -0,0 +1,91 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // a state represents the pending subtree for verification
+    // We are currently checking whether this subtree of Tree 1 can be made equivalent to this subtree of Tree 2
+
+    // (L, R) is a state :  it means compare the subtree rooted at L and rooted at R
+    struct State{
+            TreeNode* L;
+            TreeNode* R;
+        };
+    bool isFlippedOrNot(TreeNode* L, TreeNode* R){
+        // if both of them are not present
+         if (!L && !R)
+            return true;
+
+        // if only one of them is absent
+        if (!L || !R)
+            return false;
+
+        // check the value if they are equal
+        return L->val == R->val;
+    }
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        // if both of them are absent
+        if(!root1 && !root2) return true;
+        // if only one of them are absent
+        if(!root1 || !root2) return false;
+        // stack represents the node that we have discovered but yet to process
+        stack<State>st;
+        st.push({root1,root2});
+        // until all the nodes gets processed
+        while(!st.empty()){
+        State curr = st.top();
+        TreeNode* L = curr.L;
+        TreeNode* R = curr.R;
+        st.pop();  
+
+        // if both nodes doesn't exist
+        if(!L && !R) continue;
+        // if only one of them exists
+        if(!L || !R) return false;
+        // if they both exists but don't have the same value
+        if(L->val != R->val) return false;
+            /*
+                At this point we have two possible alignments:
+                1. No flip:
+                       L->left  <-> R->left
+                       L->right <-> R->right
+                2. Flip:
+                       L->left  <-> R->right
+                       L->right <-> R->left
+            */
+            bool noFlip =
+                isFlippedOrNot(L->left, R->left) &&
+                isFlippedOrNot(L->right, R->right);
+            bool flip =
+                isFlippedOrNot(L->left, R->right) &&
+                isFlippedOrNot(L->right, R->left);
+
+            if (!noFlip && !flip)
+                return false;
+
+            /*
+                If both are possible, we cannot simply choose one
+                because the deeper structure may invalidate that choice.
+                So we preserve both possibilities.
+            */
+            if (noFlip) {
+                st.push({L->left, R->left});
+                st.push({L->right, R->right});
+            }
+
+            if (flip) {
+                st.push({L->left, R->right});
+                st.push({L->right, R->left});
+            }
+        }
+    return true;
+    }
+};
