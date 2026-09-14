07f4483b95e1701691b837cc8313a5ae7494c0ab
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-09-14 12:29:49 -0700

    Time: 24 ms (66.51%) | Memory: 88.5 MB (5.29%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-    bool canDistribute(vector<int>& candies, int p, long long k) {
-        // how many complete groups we can make of group size k
-        // that determines how many children we can distribute the current number of candies to from the current candies[i] pile
+    bool canGive(vector<int>& candies, long long mid, long long k){
         int n = candies.size();
         long long cnt = 0;
-        for (int i = 0; i < n; i++) {
-            // can we atleast have p candies from each candies[i] pile
-            // count the number of complete groups 
-            cnt += floor(candies[i] / p);
+        for(int i = 0; i < n ; i++){
+            // we have to check how many children this pile can satisfy
+            // for that count the complete groups of that pile while dividing by the mid
+            // e.g candies = [10], mid = 3, 10/3 = 3 -> 3+3+3 = almost 10, so we are checking how many children can have exactly 3 candies
+            cnt += candies[i] / mid;
+            
         }
         return cnt >= k;
     }
     int maximumCandies(vector<int>& candies, long long k) {
-        // the search space belongs from the 1 to max element
-        // we can atleast distrbute one candy from each of the candies[i] piles
-        // as per the constraints
-        /*
-        1 <= candies.length <= 105
-        1 <= candies[i] <= 107
-        1 <= k <= 1012
-        */
-        int low = 1;
-        int high = *max_element(candies.begin(), candies.end());
+        int n = candies.size();
+        // we can assign 1 candy to a child at minimum
+        // at max we can assign the max pile among the candies to a child
+        long long low = 1;
+        long long high = *max_element(candies.begin(), candies.end());
         int ans = 0;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            // if it works we can check if there is a larger value if which can
-            // work so we eliminate the left search space
-            if (canDistribute(candies, mid, k)) {
+        while(low <= high){
+            long long mid = low + (high - low) / 2;
+            // if the candidate works we look for a larger one
+            // since we are trying to maximize the answer
+            if(canGive(candies, mid, k)){
                 ans = mid;
                 low = mid + 1;
-            }
-            // if it doesn't work then the ammount of candies is large
-            // we should check for a smaller value
-            else {
-                high = mid - 1;
-            }
+            } 
+            // otherwise we look for smaller one
+            else high = mid - 1;
         }
         return ans;
     }
