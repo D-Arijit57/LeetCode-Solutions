b7942f5191c4cc29f42cefaa362dddc494b1e0e0
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-09-16 09:17:37 -0700

    Time: 97 ms (5.07%), Space: 62.2 MB (5.72%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,54 +10,53 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // incase of empty tree
+        // iterative approach 
+        // // We build a parent map for every node.
+        // Using it, we can trace the ancestor chain of P and Q.
+        // the first common ancestor becomes the LCA 
+        // as that is the diversion point of P and Q
         if(root == nullptr) return nullptr;
-        // check if the current node is one of p or q 
         if(root == p || root == q) return root;
-        // what makes a node 'curr' the LCA:
-        // A node 'curr' is called the LCA of P and Q if P and Q both are contained in the subtree rooted at curr while curr being the point of diversion for the paths of P and Q or curr itself is one of P and Q
 
-        // there two cases :
-        // 1. P and Q are in the different subtrees : the common point that connect both of the subtrees becomes the LCA
-        // 2. P and Q are in the same subtree : the node placed at the higher level becomes the LCA between P and Q
-
-        // we have to somehow figure out if we are based on the same subtree or different
-        // based on that we find the LCA
-        // DFS would be a better approach for that because we are traversing to each subtree's depth first
-
-        // At each point of node (root):
-        // P and Q both have three options for existence :
-        // 1. current node 
-        // 2. left subtree
-        // 3. right subtree
-
-        // so do we know that a point is LCA :
-        // if for a node curr: p is somewhere in the left subtree and q is somewhere in the right subtree
-        // or vice versa 
-        // or curr == p or curr == q
-        // if both p and q are in the left subtree, we know this is not the diversion point so we go more left
-        // and same with if they are both happen to be in the right subtree
-        TreeNode* curr = root;
-        TreeNode* leftResult = lowestCommonAncestor(curr->left,p,q);
-        TreeNode* rightResult = lowestCommonAncestor(curr->right,p,q);
-        
-       // If both subtrees return a result,
-       // P and Q were found on opposite sides of curr,
-       // so curr is their LCA.
-        if(leftResult && rightResult){
-            return curr;
+        // parentmap : e.g parent[5] = 3
+        unordered_map<TreeNode*, TreeNode*>parentmap;
+        queue<TreeNode*>nodes;
+        nodes.push(root);
+        parentmap[root] = nullptr;
+        while(!nodes.empty()){
+            TreeNode* curr = nodes.front();
+            nodes.pop();
+            // push the left and right child
+            // while keeping a record of their parents
+            if(curr->left){
+                nodes.push(curr->left);
+                parentmap[curr->left] = curr;
+            }
+            if(curr->right){
+                nodes.push(curr->right);
+                parentmap[curr->right] = curr;
+            }
         }
-        // if only leftResult is true : means p or q somewhere exists in the left subtree  
-        // incase the LCA exists in the left subtreee
-        if(leftResult){
-            return leftResult;
+
+       // Store P and all of its ancestors 
+       unordered_set<TreeNode*> ancestors;
+       TreeNode* curr = p;
+
+       while(curr != nullptr){
+        ancestors.insert(curr);
+        curr = parentmap[curr];
+       }
+
+       // Start from Q and move upward
+       // The first node that is also an ancestor of P is the LCA
+        curr = q;
+        while(curr != nullptr){
+            if(ancestors.count(curr)){
+                return curr;
+            }
+            curr = parentmap[curr];
         }
-        // if only rightResult is true : means p or q somewhere exists in the right subtree
-        // incase the LCA exists in the right subtree
-        if(rightResult){
-            return rightResult;
-        }
-        // if both of them doesn't exist return nullptr
+
         return nullptr;
-    }   
+    }
 };
