6f5dd18b5d442100a06ced3870b08fb873a700f7
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-09-21 09:41:48 -0700

    Time: 27 ms (22.85%), Space: 27.4 MB (35.52%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // edge case : empty tree
+        if(root == nullptr) return nullptr;
+        // parent_map : represents the record of the each node and their immidate ancestor /parent
+        unordered_map<TreeNode*, TreeNode*>parent_map;
+        // using a BFS Approach
+        queue<TreeNode*>nodes;
+        nodes.push(root);
+        // initial value of parent_map (root, nullptr)
+        parent_map[root] = nullptr;
+        while(!nodes.empty()){
+            TreeNode* curr = nodes.front();
+            nodes.pop();
+            // iterate every node and store 
+            // each node with its immediate parent (ancestor)
+            // for both left and right subtree
+            if(curr->left){
+                parent_map[curr->left] = curr;
+                nodes.push(curr->left);
+            }
+            if(curr->right){
+                parent_map[curr->right] = curr;
+                nodes.push(curr->right);
+            }
+        }
+        // trace back from p and q
+        // record their ancestors
+        // the first meeting point becomes the LCA 
+        TreeNode* curr = p;
+        unordered_set<TreeNode*>ancestors;
+        while(curr){
+            ancestors.insert(curr);
+            // set to curr to the parent of of the current node (we're tracing back)
+            curr = parent_map[curr];
+           
+        }
+        
+        curr = q;
+        while(curr){
+           // check if this is the meeting point
+           if(ancestors.count(curr)) return curr;
+           // if not check the next ancestor using the parent map 
+           curr = parent_map[curr];
+           
+        }
+        return nullptr;
+    }
+};
