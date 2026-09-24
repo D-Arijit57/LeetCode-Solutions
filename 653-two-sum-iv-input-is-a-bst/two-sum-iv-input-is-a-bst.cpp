2a98220dbc5f91a04f8c7fa9400f55df09ef6baf
Arijit Das <113751255+D-Arijit57@users.noreply.github.com>
2026-09-24 10:41:20 -0700

    Time: 12 ms (12.17%) | Memory: 39.7 MB (9.02%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -12,42 +12,31 @@
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
-        // everything in leftsubtree < current node < everything in right subtree
-        // so at each point we can decide whether go left or right
-        // at current node we are going calculate the required half i.e target - curr->val
-        // and we are going to check based on if that required value is greater than the current node or not
+        // optimized solution
+        // keep an record of the pair (required) that we have encountered in a set
+        // if a the required exists it'll fulfill the condition of its other half
         if(root == nullptr) return false;
-        queue<TreeNode*>levels;
-        levels.push(root);
-        // check for each of the node 
-        while(!levels.empty()){
-            TreeNode* curr = levels.front();
-            levels.pop();
+        
+        // using stack for DFS
+        stack<TreeNode*>st;
+        st.push(root);
+
+        // hash set contains values whose complementary partner we're still looking for 
+        unordered_set<int>seen;
+
+        while(!st.empty()){
+            TreeNode* curr = st.top();
+            st.pop();
+
             int required = k - curr->val;
+            // if the other half already exists then return directly
+            if(seen.count(required)) return true;
+            // other wise record if in case we encounter its complement in future
+            seen.insert(curr->val);
 
-            // current candidate starts from the root
-            // uses BST Property to check whether the required exists in the subtree or not 
-            TreeNode* candidate = root;
-
-            while(candidate){ 
-            // make sure we don't return based on just the value 
-            // but we have to re check if we have another unique node by comparing their respective addresses
-            if(required == candidate->val &&  candidate != curr){
-                return true;
-            }
-            // if the required value is greater than the current node's value then it probably exists in the right subtree
-            if(required > candidate->val) candidate = candidate->right;
-            // if its smaller then it probably exists in the left subtree
-            else candidate = candidate->left;
-            }
-
-            // push the next left and right subtrees
-            if(curr->left){
-                levels.push(curr->left);
-            }
-            if(curr->right){
-                levels.push(curr->right);
-            }
+            // pre order DFS
+            if(curr->left)  st.push(curr->left);
+            if(curr->right) st.push(curr->right);
         }
         return false;
     }
